8be553dd0e0c8898adee32ab7b9ec3d57b97eee5
graduateDesign <graduateDesign@example.com>
2021-08-22 09:20:54 +0800

    use pika malloc in datatest

--- src/test/dataTest.c
+++ src/test/dataTest.c
@@ -4,7 +4,7 @@
 
 static void deinit(dataTest_t *self)
 {
-    DynMemPut(self->mem);
+    pikaFree(self, self->memSize);
 }
 
 static void init(dataTest_t *self, Args *args)
@@ -24,9 +24,8 @@ static void init(dataTest_t *self, Args *args)
 
 dataTest_t *New_dataTest(Args *args)
 {
-    DMEM *mem = DynMemGet(sizeof(dataTest_t));
-    dataTest_t *self = (void *)(mem->addr);
-    self->mem = mem;
+    dataTest_t *self = pikaMalloc(sizeof(dataTest_t));
+    self->memSize = sizeof(dataTest_t);
     self->init = init;
     self->init(self, args);
     return self;
