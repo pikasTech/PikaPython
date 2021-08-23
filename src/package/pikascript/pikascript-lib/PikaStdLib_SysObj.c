6c93983958a5812fdd790ba765c4c0d99f646280
lyon <lyon@catgit.com>
2021-08-23 15:03:06 +0800

    add lib

---
+++ src/package/pikascript/pikascript-lib/PikaStdLib_SysObj.c
@@ -0,0 +1,17 @@
+#include "../pikascript-core/PikaObj.h"
+
+void PikaStdLib_SysObj_ls(PikaObj *self, char *objPath)
+{
+}
+
+void PikaStdLib_SysObj_new(PikaObj *self, char *classPath, char *objPath)
+{
+}
+
+void PikaStdLib_SysObj_remove(PikaObj *self, char *argPath)
+{
+}
+
+void PikaStdLib_SysObj_type(PikaObj *self, char *argPath)
+{
+}
