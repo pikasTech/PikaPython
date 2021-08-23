d4bd4383bb8d8905b053f0e6e22b13098b4e492d
lyon <lyon@catgit.com>
2021-08-23 14:48:42 +0800

    add api

--- src/package/pikascript/PikaStdLib.py
+++ src/package/pikascript/PikaStdLib.py
@@ -4,3 +4,15 @@ from PikaObj import *
 class SysObj(BaseObj):
     def type(argPath: str):
         pass
+
+    def ls(objPath: str):
+        pass
+
+    def remove(argPath: str):
+        pass
+
+    def type(argPath: str):
+        pass
+
+    def new(objPath: str, classPath: str):
+        pass
