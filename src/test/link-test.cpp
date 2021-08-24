98ebea794e3d28b911c26afda40bb9d781c83c36
lyon <645275593@qq.com>
2021-08-24 14:00:08 +0800

    contant to content

--- src/test/link-test.cpp
+++ src/test/link-test.cpp
@@ -14,13 +14,13 @@ TEST(link_test, test1)
 
     data1->a = 2;
     data1->b = 5;
-    link_addNode(link, data1, (contantDeinitFun)data1->deinit);
+    link_addNode(link, data1, (contentDeinitFun)data1->deinit);
     data2->a = 4;
     data2->b = 21;
-    link_addNode(link, data2, (contantDeinitFun)data2->deinit);
+    link_addNode(link, data2, (contentDeinitFun)data2->deinit);
 
-    dataTest_t *data1InLink = (dataTest_t *)link_getNode(link, 0)->contant;
-    dataTest_t *data2InLink = (dataTest_t *)link_getNode(link, 1)->contant;
+    dataTest_t *data1InLink = (dataTest_t *)link_getNode(link, 0)->content;
+    dataTest_t *data2InLink = (dataTest_t *)link_getNode(link, 1)->content;
     EXPECT_EQ(2, data1InLink->a);
     EXPECT_EQ(5, data1InLink->b);
     EXPECT_EQ(4, data2InLink->a);
