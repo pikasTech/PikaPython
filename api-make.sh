6859f9f8ee79dad3e141b96f44f04f4345edf1f0
graduateDesign <graduateDesign@example.com>
2021-08-25 09:39:32 +0800

    add win-compiler.exe

--- api-make.sh
+++ api-make.sh
@@ -1,2 +1,3 @@
 cd src/package/pikascript
 ./rust-msc-latest
+./rust-msc-latest.exe
--- update-compiler.sh
+++ update-compiler.sh
@@ -3,3 +3,4 @@ cd ../pikascript-compiler-rust
 cargo build
 cd $ROOT
 cp ../pikascript-compiler-rust/target/debug/rust-msc src/package/pikascript/rust-msc-latest
+cp ../pikascript-compiler-rust/target/debug/rust-msc.exe src/package/pikascript/rust-msc-latest.exe
