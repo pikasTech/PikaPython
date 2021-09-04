726009aa538e35810e0bcc92f4338fc2ab8a88d7
lyon <catgit.@catgit.com>
2021-09-04 12:36:02 +0800

    testing block assert

--- src/package/pikascript/pikascript-core/PikaBlock.h
+++ src/package/pikascript/pikascript-core/PikaBlock.h
@@ -6,4 +6,7 @@ void block_deinit(PikaObj *self);
 PikaObj *block_init();
 char *block_popLine(PikaObj *self);
 void block_pushLine(PikaObj *self, char *line);
+char *block_getAssert(PikaObj *self);
+void block_setAssert(PikaObj *self, char *assert);
+uint8_t block_checkAssert(PikaObj *self);
 #endif
