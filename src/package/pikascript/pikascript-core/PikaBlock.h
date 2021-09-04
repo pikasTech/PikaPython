1d981493889a255bfe75e1d4e68904760d330c2a
lyon <catgit.@catgit.com>
2021-09-05 00:21:16 +0800

    tested pass, adding block in run_derict

--- src/package/pikascript/pikascript-core/PikaBlock.h
+++ src/package/pikascript/pikascript-core/PikaBlock.h
@@ -9,4 +9,7 @@ void block_pushLine(PikaObj *self, char *line);
 char *block_getAssert(PikaObj *self);
 void block_setAssert(PikaObj *self, char *assert);
 uint8_t block_checkAssert(PikaObj *self);
+uint16_t block_getLineSize(PikaObj *self);
+void block_setMode(PikaObj *self, char *mode);
+char *block_getMode(PikaObj *self);
 #endif
