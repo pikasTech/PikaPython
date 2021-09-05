2cf56f11f146f76e46654aa9c83af191464d1063
lyon <lyon@catgit.com>
2021-09-05 14:36:36 +0800

    use compier in main repo

--- update-compiler.sh
+++ update-compiler.sh
@@ -1,6 +1,6 @@
 ROOT=$PWD
-cd ../pikascript-compiler-rust
+cd pikascript-compiler-rust
 cargo build
 cd $ROOT
-cp ../pikascript-compiler-rust/target/debug/rust-msc src/package/pikascript/rust-msc-latest.exe
-cp ../pikascript-compiler-rust/target/debug/rust-msc.exe src/package/pikascript/rust-msc-latest.exe
+cp pikascript-compiler-rust/target/debug/rust-msc src/package/pikascript/rust-msc-latest.exe
+cp pikascript-compiler-rust/target/debug/rust-msc.exe src/package/pikascript/rust-msc-latest.exe
