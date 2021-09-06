f7f0655d801cbf6f616cae5b134ba87d811a0193
graduateDesign <graduateDesign@example.com>
2021-09-06 17:40:27 +0800

    add stm32g070

--- demo/stm32g070/Core/Src/main.c
+++ demo/stm32g070/Core/Src/main.c
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "pikaScript.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -88,7 +88,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  PikaObj *pikaMain = pikaScriptInit();
   /* USER CODE END 2 */
 
   /* Infinite loop */
