708dd7aee15538357b40a286969ecfd7bb48e987
graduateDesign <graduateDesign@example.com>
2021-09-08 21:26:36 +0800

    updelete

--- demo/stm32g070/pikascript/pikascript-core/PikaWhile.h
+++
@@ -1,9 +0,0 @@
-#ifndef _pikaWhile__H
-#define _pikaWhile__H
-#include "PikaObj.h"
-
-void while_setAssert(PikaObj *self, char *line);
-void while_pushLine(PikaObj *self, char *line);
-void while_run(PikaObj *self);
-
-#endif
