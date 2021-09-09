3107b1154fcbe7e73848eb723d15ce58031590d8
graduateDesign <graduateDesign@example.com>
2021-09-09 08:40:13 +0800

    adc driver is ok

--- demo/stm32g070cbt6/pikascript/main.py
+++ demo/stm32g070cbt6/pikascript/main.py
@@ -4,12 +4,17 @@ import STM32
 mem = PikaStdLib.MemChecker()
 io1 = STM32.GPIO()
 time = STM32.Time()
+adc1 = STM32.ADC()
 
 io1.init()
 io1.setPin('PB4')
 io1.setMode('out')
 io1.enable()
 io1.low()
+
+adc1.init()
+adc1.setPin('PA1')
+adc1.enable()
 
 print('hello pikascript')
 print('mem.max :')
@@ -22,3 +27,6 @@ while True:
     time.sleep_ms(500)
     io1.high()
     time.sleep_ms(500)
+    val = adc1.read()
+    print('adc1 value:')
+    print(val)
