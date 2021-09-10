5f5b091027646ff3761016cf46c5d370e9bdf017
lyon <catgit.@catgit.com>
2021-09-10 18:14:09 +0800

    fromat core

--- src/package/pikascript/pikascript-core/PikaInvoke.h
+++ src/package/pikascript/pikascript-core/PikaInvoke.h
@@ -1,4 +1,4 @@
 #ifndef _invoke__H
 #define _invoke__H
-Args *obj_invoke(PikaObj *self, char *cmd);
+Args* obj_invoke(PikaObj* self, char* cmd);
 #endif
--- src/package/pikascript/pikascript-core/dataMemory.h
+++ src/package/pikascript/pikascript-core/dataMemory.h
@@ -5,14 +5,13 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct
-{
-    uint32_t heapUsed;
-    uint32_t heapUsedMax;
+typedef struct {
+  uint32_t heapUsed;
+  uint32_t heapUsedMax;
 } PikaMemInfo;
 
-void pikaFree(void *mem, uint32_t size);
-void *pikaMalloc(uint32_t size);
+void pikaFree(void* mem, uint32_t size);
+void* pikaMalloc(uint32_t size);
 uint16_t pikaMemNow(void);
 uint16_t pikaMemMax(void);
 void pikaMemMaxReset(void);
