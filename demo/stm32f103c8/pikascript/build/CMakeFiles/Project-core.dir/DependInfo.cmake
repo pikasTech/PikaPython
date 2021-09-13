d890d50954fff44db74ac95980e7f8e78f97ca9d
graduateDesign <graduateDesign@example.com>
2021-09-13 23:07:03 +0800

    stm32f103c8 device is ok

---
+++ demo/stm32f103c8/pikascript/build/CMakeFiles/Project-core.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/git/pikascript/demo/stm32g070/pikascript/build/CMakeFiles/3.21.1/CompilerIdC/CMakeCCompilerId.c" "CMakeFiles/Project-core.dir/CMakeFiles/3.21.1/CompilerIdC/CMakeCCompilerId.obj" "gcc" "CMakeFiles/Project-core.dir/CMakeFiles/3.21.1/CompilerIdC/CMakeCCompilerId.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/PikaMain-api.c" "CMakeFiles/Project-core.dir/pikascript-api/PikaMain-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/PikaMain-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/PikaStdDevice_GPIO-api.c" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdDevice_GPIO-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdDevice_GPIO-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/PikaStdDevice_Time-api.c" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdDevice_Time-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdDevice_Time-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/PikaStdLib_MemChecker-api.c" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdLib_MemChecker-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdLib_MemChecker-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/PikaStdLib_SysObj-api.c" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdLib_SysObj-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/PikaStdLib_SysObj-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/STM32_GPIO-api.c" "CMakeFiles/Project-core.dir/pikascript-api/STM32_GPIO-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/STM32_GPIO-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/STM32_Time-api.c" "CMakeFiles/Project-core.dir/pikascript-api/STM32_Time-api.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/STM32_Time-api.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-api/pikaScript.c" "CMakeFiles/Project-core.dir/pikascript-api/pikaScript.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-api/pikaScript.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/BaseObj.c" "CMakeFiles/Project-core.dir/pikascript-core/BaseObj.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/BaseObj.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/PikaBlock.c" "CMakeFiles/Project-core.dir/pikascript-core/PikaBlock.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/PikaBlock.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/PikaIf.c" "CMakeFiles/Project-core.dir/pikascript-core/PikaIf.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/PikaIf.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/PikaInvoke.c" "CMakeFiles/Project-core.dir/pikascript-core/PikaInvoke.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/PikaInvoke.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/PikaObj.c" "CMakeFiles/Project-core.dir/pikascript-core/PikaObj.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/PikaObj.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/PikaWhile.c" "CMakeFiles/Project-core.dir/pikascript-core/PikaWhile.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/PikaWhile.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/TinyObj.c" "CMakeFiles/Project-core.dir/pikascript-core/TinyObj.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/TinyObj.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataArg.c" "CMakeFiles/Project-core.dir/pikascript-core/dataArg.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataArg.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataArgs.c" "CMakeFiles/Project-core.dir/pikascript-core/dataArgs.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataArgs.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataLink.c" "CMakeFiles/Project-core.dir/pikascript-core/dataLink.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataLink.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataLinkNode.c" "CMakeFiles/Project-core.dir/pikascript-core/dataLinkNode.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataLinkNode.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataMemory.c" "CMakeFiles/Project-core.dir/pikascript-core/dataMemory.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataMemory.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataString.c" "CMakeFiles/Project-core.dir/pikascript-core/dataString.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataString.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/dataStrs.c" "CMakeFiles/Project-core.dir/pikascript-core/dataStrs.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/dataStrs.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-core/method.c" "CMakeFiles/Project-core.dir/pikascript-core/method.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-core/method.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-lib/PikaStdDevice/PikaStdDeivce_GPIO.c" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdDevice/PikaStdDeivce_GPIO.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdDevice/PikaStdDeivce_GPIO.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-lib/PikaStdDevice/PikaStdDevice_Time.c" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdDevice/PikaStdDevice_Time.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdDevice/PikaStdDevice_Time.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-lib/PikaStdLib/PikaStdLib_MemChecker.c" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdLib/PikaStdLib_MemChecker.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdLib/PikaStdLib_MemChecker.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-lib/PikaStdLib/PikaStdLib_SysObj.c" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdLib/PikaStdLib_SysObj.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-lib/PikaStdLib/PikaStdLib_SysObj.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-lib/STM32/STM32_GPIO.c" "CMakeFiles/Project-core.dir/pikascript-lib/STM32/STM32_GPIO.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-lib/STM32/STM32_GPIO.obj.d"
+  "D:/git/pikascript/demo/stm32g070/pikascript/pikascript-lib/STM32/STM32_Time.c" "CMakeFiles/Project-core.dir/pikascript-lib/STM32/STM32_Time.obj" "gcc" "CMakeFiles/Project-core.dir/pikascript-lib/STM32/STM32_Time.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
