2b701360266c1c2dee55cb54f26097f80ef3d0e3
Lyon <liang6516@outlook.com>
2021-09-18 22:28:59 +0800

    add std device to core

--- demo/stm32g030c8/pikascript/main.py
+++ demo/stm32g030c8/pikascript/main.py
@@ -1,26 +1,5 @@
-import PikaStdLib
 import STM32
-
-mem = PikaStdLib.MemChecker()
-
 time = STM32.Time()
-
-uart = STM32.UART()
-uart.init()
-uart.setBaudRate(115200)
-uart.setId(1)
-uart.enable()
-
-io1 = STM32.GPIO()
-io1.init()
-io1.setPin('PC13')
-io1.low()
-io1.enable()
-
-adc = STM32.ADC()
-adc.init()
-adc.setPin('PA0')
-adc.enable()
 
 pwm = STM32.PWM()
 pwm.setPin('PA8')
@@ -30,18 +9,7 @@ pwm.enable()
 
 while True:
     time.sleep_ms(500)
-    io1.low()
-    readBuff = uart.read(2)
-    print('mem used max:')
-    mem.max()
-    print('read 2 char:')
-    print(readBuff)
-    print(' ')
     pwm.setDuty(0.5)
 
     time.sleep_ms(500)
-    io1.high()
-    adcVal = adc.read()
-    print('adc value:')
-    print(adcVal)
     pwm.setDuty(0.3)
