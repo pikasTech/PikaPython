f722c6b12825afa167f447143030947d9a634f62
Lyon <liang6516@outlook.com>
2021-09-18 22:09:26 +0800

    pwm for stdDevice and g030 is ok

--- demo/stm32g030c8/pikascript/pikascript-api/pikaScript.c
+++ demo/stm32g030c8/pikascript/pikascript-api/pikaScript.c
@@ -18,8 +18,12 @@ PikaObj * pikaScriptInit(){
     obj_run(pikaMain, "adc.init()");
     obj_run(pikaMain, "adc.setPin('PA0')");
     obj_run(pikaMain, "adc.enable()");
+    obj_run(pikaMain, "pwm.setPin('PA8')");
+    obj_run(pikaMain, "pwm.setFrequency(2000)");
+    obj_run(pikaMain, "pwm.setDuty(0.5)");
+    obj_run(pikaMain, "pwm.enable()");
     obj_run(pikaMain, "while True:");
-    obj_run(pikaMain, "    time.sleep_ms(50)");
+    obj_run(pikaMain, "    time.sleep_ms(500)");
     obj_run(pikaMain, "    io1.low()");
     obj_run(pikaMain, "    readBuff = uart.read(2)");
     obj_run(pikaMain, "    print('mem used max:')");
@@ -27,11 +31,13 @@ PikaObj * pikaScriptInit(){
     obj_run(pikaMain, "    print('read 2 char:')");
     obj_run(pikaMain, "    print(readBuff)");
     obj_run(pikaMain, "    print(' ')");
-    obj_run(pikaMain, "    time.sleep_ms(50)");
+    obj_run(pikaMain, "    pwm.setDuty(0.5)");
+    obj_run(pikaMain, "    time.sleep_ms(500)");
     obj_run(pikaMain, "    io1.high()");
     obj_run(pikaMain, "    adcVal = adc.read()");
     obj_run(pikaMain, "    print('adc value:')");
     obj_run(pikaMain, "    print(adcVal)");
+    obj_run(pikaMain, "    pwm.setDuty(0.3)");
     obj_run(pikaMain, "");
     return pikaMain;
 }
--- demo/stm32g030c8/pikascript/pikascript-lib/STM32/STM32_PWM.c
+++ demo/stm32g030c8/pikascript/pikascript-lib/STM32/STM32_PWM.c
@@ -76,23 +76,224 @@ static TIM_TypeDef* getTimInstance(char *pin)
         if( strEqu("PD1", pin) ){
             return TIM17;
         }
-    #endif    
+    #endif
     return NULL;
 }
 
-uint8_t PWM_MspPostInit(char* pin){
-    if (NULL == getTimInstance(pin))
-    {   
+static uint32_t getGPIO_AlternateForTim(TIM_TypeDef *timInstance){
+    #ifdef TIM1_EXIST
+        if( TIM1 == timInstance ){
+            return GPIO_AF2_TIM1;
+        }
+    #endif
+    #ifdef TIM3_EXIST
+        if( TIM3 == timInstance ){
+            return GPIO_AF1_TIM3;
+        }
+    #endif
+    #ifdef TIM14_EXIST
+        if( TIM14 == timInstance ){
+            return GPIO_AF4_TIM14;
+        }
+    #endif 
+    #ifdef TIM16_EXIST
+        if( TIM16 == timInstance ){
+            return GPIO_AF2_TIM16;
+        }
+    #endif      
+    #ifdef TIM17_EXIST
+        if( TIM17 == timInstance ){
+            return GPIO_AF2_TIM17;
+        }
+    #endif           
+    return 0;
+}
+
+static void PWM_TimClockEnable(TIM_TypeDef *timInstance){
+    #ifdef TIM1_EXIST
+        if( TIM1 == timInstance ){
+            __HAL_RCC_TIM1_CLK_ENABLE();
+            return;
+        }
+    #endif
+    #ifdef TIM3_EXIST
+        if( TIM3 == timInstance ){
+            __HAL_RCC_TIM3_CLK_ENABLE();
+            return;
+        }
+    #endif
+    #ifdef TIM14_EXIST
+        if( TIM14 == timInstance ){
+            __HAL_RCC_TIM14_CLK_ENABLE();
+            return;
+        }
+    #endif 
+    #ifdef TIM16_EXIST
+        if( TIM16 == timInstance ){
+            __HAL_RCC_TIM16_CLK_ENABLE();
+            return;
+        }
+    #endif      
+    #ifdef TIM17_EXIST
+        if( TIM17 == timInstance ){
+            __HAL_RCC_TIM17_CLK_ENABLE();
+            return;
+        }
+    #endif           
+}
+
+uint8_t PWM_MspInit(char* pin){
+    TIM_TypeDef *timInstance = getTimInstance(pin);
+    if (NULL == timInstance){   
         /* this Pin do not match any PWM generator */
         return 1;
     }
     enableClk(pin);
+    GPIO_InitTypeDef GPIO_InitStruct = {0};    
+    GPIO_InitStruct.Pin = getGpioPin(pin);
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = getGPIO_AlternateForTim(timInstance);
+    HAL_GPIO_Init(getGpioPort(pin), &GPIO_InitStruct);
+    PWM_TimClockEnable(timInstance);
     return 0;
 }
 
+uint32_t getTimChennel(char *pin){
+    if (strEqu("PA8", pin)   || 
+        strEqu("PA6", pin)   || 
+        strEqu("PA4", pin)   || 
+        strEqu("PD0", pin)   ||
+        strEqu("PD1", pin) ){
+        return TIM_CHANNEL_1;
+    }
+    if (strEqu("PA9", pin)   || 
+        strEqu("PA7", pin) ){
+        return TIM_CHANNEL_2;
+    }
+    if (strEqu("PA10", pin)   || 
+        strEqu("PB0", pin) ){
+        return TIM_CHANNEL_3;
+    }
+    if (strEqu("PA11", pin)   || 
+        strEqu("PB1", pin) ){
+        return TIM_CHANNEL_4;
+    }
+    /* Chennel not match */
+    return 99999;
+}
+
 void STM32_PWM_platformEnable(PikaObj *self, float duty, int freq, char * pin){
+    TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+    TIM_MasterConfigTypeDef sMasterConfig = {0};
+    TIM_OC_InitTypeDef sConfigOC = {0};
+    TIM_BreakDeadTimeConfigTypeDef sBreakDeadTimeConfig = {0};
+
+    if (0 != PWM_MspInit(pin)){
+        obj_setSysOut(self, "[error]: init PWM port faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+
+    TIM_HandleTypeDef *pika_tim = getTimHandle(pin);
+    if (NULL == pika_tim){
+        obj_setSysOut(self, "[error]: can not found PWM hardware.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+
+    pika_tim->Instance = getTimInstance(pin);
+    pika_tim->Init.Prescaler = 64-1;
+    pika_tim->Init.CounterMode = TIM_COUNTERMODE_UP;
+    /* calculate period */    
+    pika_tim->Init.Period = (uint32_t)( (float)(1000 * 1000)/(float)freq ) - 1;
+    pika_tim->Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    pika_tim->Init.RepetitionCounter = 0;
+    pika_tim->Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+    
+    if (HAL_TIM_Base_Init(pika_tim) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+    if (HAL_TIM_ConfigClockSource(pika_tim, &sClockSourceConfig) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    if (HAL_TIM_PWM_Init(pika_tim) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    if (HAL_TIM_OC_Init(pika_tim) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterOutputTrigger2 = TIM_TRGO2_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    if (HAL_TIMEx_MasterConfigSynchronization(pika_tim, &sMasterConfig) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;
+    /* calculate pulse by duty and freq */
+    sConfigOC.Pulse = (uint32_t)(pika_tim->Init.Period * duty);
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+    sConfigOC.OCIdleState = TIM_OCIDLESTATE_RESET;
+    sConfigOC.OCNIdleState = TIM_OCNIDLESTATE_RESET;
+    if (HAL_TIM_PWM_ConfigChannel(pika_tim, &sConfigOC, getTimChennel(pin)) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+
+    sBreakDeadTimeConfig.OffStateRunMode = TIM_OSSR_DISABLE;
+    sBreakDeadTimeConfig.OffStateIDLEMode = TIM_OSSI_DISABLE;
+    sBreakDeadTimeConfig.LockLevel = TIM_LOCKLEVEL_OFF;
+    sBreakDeadTimeConfig.DeadTime = 0;
+    sBreakDeadTimeConfig.BreakState = TIM_BREAK_DISABLE;
+    sBreakDeadTimeConfig.BreakPolarity = TIM_BREAKPOLARITY_HIGH;
+    sBreakDeadTimeConfig.BreakFilter = 0;
+    sBreakDeadTimeConfig.BreakAFMode = TIM_BREAK_AFMODE_INPUT;
+    sBreakDeadTimeConfig.Break2State = TIM_BREAK2_DISABLE;
+    sBreakDeadTimeConfig.Break2Polarity = TIM_BREAK2POLARITY_HIGH;
+    sBreakDeadTimeConfig.Break2Filter = 0;
+    sBreakDeadTimeConfig.Break2AFMode = TIM_BREAK_AFMODE_INPUT;
+    sBreakDeadTimeConfig.AutomaticOutput = TIM_AUTOMATICOUTPUT_DISABLE;
+    if (HAL_TIMEx_ConfigBreakDeadTime(pika_tim, &sBreakDeadTimeConfig) != HAL_OK){
+        obj_setSysOut(self, "[error]: init PWM faild.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    HAL_TIM_PWM_Start(pika_tim, getTimChennel(pin));
 }
+
 void STM32_PWM_platformSetDuty(PikaObj *self, float duty, char * pin){
+    TIM_HandleTypeDef *pika_tim = getTimHandle(pin);
+    if (NULL == pika_tim){
+        obj_setSysOut(self, "[error]: can not found PWM hardware.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    __HAL_TIM_SET_COMPARE( pika_tim, getTimChennel(pin), (uint32_t)(pika_tim->Init.Period * duty) );
 }
+
 void STM32_PWM_platformSetFrequency(PikaObj *self, int freq, char * pin){
+    TIM_HandleTypeDef *pika_tim = getTimHandle(pin);
+    if (NULL == pika_tim){
+        obj_setSysOut(self, "[error]: can not found PWM hardware.");
+        obj_setErrorCode(self, 1);
+        return;
+    }
+    __HAL_TIM_SET_AUTORELOAD( pika_tim, (uint32_t)( (float)(1000 * 1000)/(float)freq ) - 1);
+    float duty = obj_getFloat(self, "duty");
+    __HAL_TIM_SET_COMPARE( pika_tim, getTimChennel(pin), (uint32_t)(pika_tim->Init.Period * duty) );
 }
