cb98ae181b467caa5590ec6231d1cdf76b1e2e64
Lyon <liang6516@outlook.com>
2021-09-20 22:29:34 +0800

    add rgb

--- demo/stm32g030c8/pikascript/pikascript-api/PikaMain-api.c
+++ demo/stm32g030c8/pikascript/pikascript-api/PikaMain-api.c
@@ -3,10 +3,8 @@
 /* ******************************** */
 #include "PikaMain.h"
 #include "PikaStdLib_SysObj.h"
-#include "STM32_ADC.h"
-#include "STM32_GPIO.h"
 #include "PikaStdLib_MemChecker.h"
-#include "STM32_PWM.h"
+#include "PikaPiZero_RGB.h"
 #include "STM32_Time.h"
 #include "STM32_UART.h"
 #include <stdio.h>
@@ -15,14 +13,10 @@
 
 PikaObj *New_PikaMain(Args *args){
     PikaObj *self = New_PikaStdLib_SysObj(args);
-    obj_import(self, "STM32_ADC", New_STM32_ADC);
-    obj_newObj(self, "adc", "STM32_ADC");
-    obj_import(self, "STM32_GPIO", New_STM32_GPIO);
-    obj_newObj(self, "io1", "STM32_GPIO");
     obj_import(self, "PikaStdLib_MemChecker", New_PikaStdLib_MemChecker);
     obj_newObj(self, "mem", "PikaStdLib_MemChecker");
-    obj_import(self, "STM32_PWM", New_STM32_PWM);
-    obj_newObj(self, "pwm", "STM32_PWM");
+    obj_import(self, "PikaPiZero_RGB", New_PikaPiZero_RGB);
+    obj_newObj(self, "rgb", "PikaPiZero_RGB");
     obj_import(self, "STM32_Time", New_STM32_Time);
     obj_newObj(self, "time", "STM32_Time");
     obj_import(self, "STM32_UART", New_STM32_UART);
