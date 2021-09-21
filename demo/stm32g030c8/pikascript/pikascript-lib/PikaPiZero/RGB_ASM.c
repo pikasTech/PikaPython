aa037ba68f0ba631e2ed4c4cf6907bca150f54e7
graduateDesign <graduateDesign@example.com>
2021-09-21 12:10:28 +0800

    debug RGB_ASM

--- demo/stm32g030c8/pikascript/pikascript-lib/PikaPiZero/RGB_ASM.c
+++ demo/stm32g030c8/pikascript/pikascript-lib/PikaPiZero/RGB_ASM.c
@@ -1,37 +1,40 @@
 #include "RGB_ASM.h"
 #include "STM32_common.h"
 
+static void RGB_setUp(){
+    GPIOB->BSRR = GPIO_PIN_12;    // set
+    int i;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    i++;
+    GPIOB->BRR = GPIO_PIN_12;     // reset
+}
+
+static void RGB_setDown(){
+    GPIOB->BSRR = GPIO_PIN_12;    // set
+    int i;
+    i++;
+    i++;
+    i++;          
+    GPIOB->BRR = GPIO_PIN_12;    
+}
+
 void RGB_set(uint32_t G8R8B8)
 {
-   int i = 23;
-   uint8_t byte = 0;
-   while( i>0 ){
-       byte = ((G8R8B8 >> i) & 0x01);
-       if(byte){
-           GPIOB->BSRR = GPIO_PIN_12;    // set
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           GPIOB->BRR = GPIO_PIN_12;     // reset
-       }
-       else{
-           GPIOB->BSRR = GPIO_PIN_12;    // set
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           __ASM ("nop");__ASM ("nop");__ASM ("nop");
-           GPIOB->BRR = GPIO_PIN_12;    
-       }
-       i -- ;
-   }
+    int i;
+    uint8_t byte = 0;
+    for(i = 23; i>=0 ;i--){
+        byte = ((G8R8B8 >> i) & 0x01);
+        if(byte){
+            RGB_setUp();
+        }
+        else{
+            RGB_setDown();
+        }
+    }
 }
