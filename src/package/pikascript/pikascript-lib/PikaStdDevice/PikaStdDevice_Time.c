1bbc9e4015a7ca5e57d4194a185ec9f13bfd43f2
Lyon <liang6516@outlook.com>
2021-09-22 16:07:26 +0800

    format stdlib

--- src/package/pikascript/pikascript-lib/PikaStdDevice/PikaStdDevice_Time.c
+++ src/package/pikascript/pikascript-lib/PikaStdDevice/PikaStdDevice_Time.c
@@ -2,10 +2,10 @@
 #include "BaseObj.h"
 
 void PikaStdDevice_Time_sleep_ms(PikaObj* self, int ms) {
-  obj_setErrorCode(self, 1);
-  obj_setSysOut(self, "[error] platform method need to be override.");
+    obj_setErrorCode(self, 1);
+    obj_setSysOut(self, "[error] platform method need to be override.");
 }
 void PikaStdDevice_Time_sleep_s(PikaObj* self, int s) {
-  obj_setErrorCode(self, 1);
-  obj_setSysOut(self, "[error] platform method need to be override.");
+    obj_setErrorCode(self, 1);
+    obj_setSysOut(self, "[error] platform method need to be override.");
 }
