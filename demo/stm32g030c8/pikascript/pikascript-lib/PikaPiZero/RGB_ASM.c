2d5419875ec7406c5f98a15852da8bc9a73279ce
Lyon <liang6516@outlook.com>
2021-09-22 16:10:33 +0800

    format lib for g030

--- demo/stm32g030c8/pikascript/pikascript-lib/PikaPiZero/RGB_ASM.c
+++ demo/stm32g030c8/pikascript/pikascript-lib/PikaPiZero/RGB_ASM.c
@@ -1,8 +1,8 @@
 #include "RGB_ASM.h"
 #include "STM32_common.h"
 
-static void RGB_setUp(){
-    GPIOB->BSRR = GPIO_PIN_12;    // set
+static void RGB_setUp() {
+    GPIOB->BSRR = GPIO_PIN_12;  // set
     int i;
     i++;
     i++;
@@ -12,28 +12,26 @@ static void RGB_setUp(){
     i++;
     i++;
     i++;
-    GPIOB->BRR = GPIO_PIN_12;     // reset
+    GPIOB->BRR = GPIO_PIN_12;  // reset
 }
 
-static void RGB_setDown(){
-    GPIOB->BSRR = GPIO_PIN_12;    // set
+static void RGB_setDown() {
+    GPIOB->BSRR = GPIO_PIN_12;  // set
     int i;
     i++;
     i++;
-    i++;          
-    GPIOB->BRR = GPIO_PIN_12;    
+    i++;
+    GPIOB->BRR = GPIO_PIN_12;
 }
 
-void RGB_set(uint32_t G8R8B8)
-{
+void RGB_set(uint32_t G8R8B8) {
     int i;
     uint8_t byte = 0;
-    for(i = 23; i>=0 ;i--){
+    for (i = 23; i >= 0; i--) {
         byte = ((G8R8B8 >> i) & 0x01);
-        if(byte){
+        if (byte) {
             RGB_setUp();
-        }
-        else{
+        } else {
             RGB_setDown();
         }
     }
