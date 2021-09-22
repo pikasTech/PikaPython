98162605e4ca28e7e81132fb3f61f51ef991e62a
Lyon <liang6516@outlook.com>
2021-09-22 16:10:33 +0800

    format lib for g030

--- demo/stm32g030c8/pikascript/pikascript-lib/PikaPiZero/PikaPiZero_RGB.c
+++ demo/stm32g030c8/pikascript/pikascript-lib/PikaPiZero/PikaPiZero_RGB.c
@@ -1,13 +1,13 @@
 #include "PikaPiZero_RGB.h"
+#include <stdint.h>
 #include "BaseObj.h"
 #include "RGB_ASM.h"
 #include "STM32_common.h"
 #include "dataStrs.h"
-#include <stdint.h>
 
 void RGB_reset() {
-  GPIOB->BRR = GPIO_PIN_12; // reset
-  delay_us(50);
+    GPIOB->BRR = GPIO_PIN_12;  // reset
+    delay_us(50);
 }
 
 #define RED 0x000100
@@ -16,78 +16,80 @@ void RGB_reset() {
 #define WHITE 0x010101
 #define CUTDOWN 0x000000
 
-void RGB_setVoid() { __asm("nop"); }
-
-void PikaPiZero_RGB_enable(PikaObj *self) {
-  obj_run(self, "pin.init()");
-  obj_run(self, "pin.setPin('PB12')");
-  obj_run(self, "pin.setMode('out')");
-  obj_run(self, "pin.enable()");
-}
-void PikaPiZero_RGB_init(PikaObj *self) {}
-
-void PikaPiZero_RGB_red(PikaObj *self) {
-  RGB_set(RED);
-  RGB_set(RED);
-  RGB_set(RED);
-  RGB_set(RED);
+void RGB_setVoid() {
+    __asm("nop");
 }
 
-void PikaPiZero_RGB_blue(PikaObj *self) {
-  RGB_set(BLUE);
-  RGB_set(BLUE);
-  RGB_set(BLUE);
-  RGB_set(BLUE);
+void PikaPiZero_RGB_enable(PikaObj* self) {
+    obj_run(self, "pin.init()");
+    obj_run(self, "pin.setPin('PB12')");
+    obj_run(self, "pin.setMode('out')");
+    obj_run(self, "pin.enable()");
 }
+void PikaPiZero_RGB_init(PikaObj* self) {}
 
-void PikaPiZero_RGB_green(PikaObj *self) {
-  RGB_set(GREEN);
-  RGB_set(GREEN);
-  RGB_set(GREEN);
-  RGB_set(GREEN);
-}
-
-void PikaPiZero_RGB_white(PikaObj *self) {
-  RGB_set(WHITE);
-  RGB_set(WHITE);
-  RGB_set(WHITE);
-  RGB_set(WHITE);
-}
-
-void PikaPiZero_RGB_flow(PikaObj *self) {
-  if (!obj_isArgExist(self, "flowState")) {
-    obj_setInt(self, "flowState", 0);
-  }
-  int flowState = obj_getInt(self, "flowState");
-  if (0 == flowState) {
+void PikaPiZero_RGB_red(PikaObj* self) {
     RGB_set(RED);
-    RGB_set(BLUE);
-    RGB_set(GREEN);
-    RGB_set(WHITE);
-    goto exit;
-  }
-  if (1 == flowState) {
-    RGB_set(BLUE);
-    RGB_set(GREEN);
-    RGB_set(WHITE);
     RGB_set(RED);
-    goto exit;
-  }
-  if (2 == flowState) {
-    RGB_set(GREEN);
-    RGB_set(WHITE);
     RGB_set(RED);
-    RGB_set(BLUE);
-    goto exit;
-  }
-  if (3 == flowState) {
-    RGB_set(WHITE);
     RGB_set(RED);
+}
+
+void PikaPiZero_RGB_blue(PikaObj* self) {
     RGB_set(BLUE);
+    RGB_set(BLUE);
+    RGB_set(BLUE);
+    RGB_set(BLUE);
+}
+
+void PikaPiZero_RGB_green(PikaObj* self) {
     RGB_set(GREEN);
-    goto exit;
-  }
+    RGB_set(GREEN);
+    RGB_set(GREEN);
+    RGB_set(GREEN);
+}
+
+void PikaPiZero_RGB_white(PikaObj* self) {
+    RGB_set(WHITE);
+    RGB_set(WHITE);
+    RGB_set(WHITE);
+    RGB_set(WHITE);
+}
+
+void PikaPiZero_RGB_flow(PikaObj* self) {
+    if (!obj_isArgExist(self, "flowState")) {
+        obj_setInt(self, "flowState", 0);
+    }
+    int flowState = obj_getInt(self, "flowState");
+    if (0 == flowState) {
+        RGB_set(RED);
+        RGB_set(BLUE);
+        RGB_set(GREEN);
+        RGB_set(WHITE);
+        goto exit;
+    }
+    if (1 == flowState) {
+        RGB_set(BLUE);
+        RGB_set(GREEN);
+        RGB_set(WHITE);
+        RGB_set(RED);
+        goto exit;
+    }
+    if (2 == flowState) {
+        RGB_set(GREEN);
+        RGB_set(WHITE);
+        RGB_set(RED);
+        RGB_set(BLUE);
+        goto exit;
+    }
+    if (3 == flowState) {
+        RGB_set(WHITE);
+        RGB_set(RED);
+        RGB_set(BLUE);
+        RGB_set(GREEN);
+        goto exit;
+    }
 
 exit:
-  obj_setInt(self, "flowState", (flowState + 1) % 4);
+    obj_setInt(self, "flowState", (flowState + 1) % 4);
 }
