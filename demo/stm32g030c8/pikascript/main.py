8d772e430ec8ce36f7a1aa1f55f99642cb06ed12
graduateDesign <graduateDesign@example.com>
2021-09-26 23:27:20 +0800

    new all object once

--- demo/stm32g030c8/pikascript/main.py
+++ demo/stm32g030c8/pikascript/main.py
@@ -1,23 +1,13 @@
 import STM32
 import PikaPiZero
 import PikaStdLib
+
 time = STM32.Time()
 uart = STM32.UART()
+adc = STM32.ADC()
+pin = STM32.GPIO()
+pwm = STM32.PWM()
+
 rgb = PikaPiZero.RGB()
 mem = PikaStdLib.MemChecker()
 
-uart.init()
-uart.setId(1)
-uart.setBaudRate(115200)
-uart.enable()
-
-print('initing rgb...')
-rgb.init()
-rgb.enable()
-print('init rgb ok!')
-
-print('mem max:')
-mem.max()
-while True:
-    time.sleep_ms(50)
-    rgb.flow()
