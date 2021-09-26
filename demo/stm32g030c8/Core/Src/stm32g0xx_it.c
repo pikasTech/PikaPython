c15e7073e270204aab585db5a12ddf57599b466b
Lyon <liang6516@outlook.com>
2021-09-26 12:06:28 +0800

    use realloc to imporve stability

--- demo/stm32g030c8/Core/Src/stm32g0xx_it.c
+++ demo/stm32g030c8/Core/Src/stm32g0xx_it.c
@@ -84,7 +84,7 @@ void NMI_Handler(void) {
  */
 void HardFault_Handler(void) {
     /* USER CODE BEGIN HardFault_IRQn 0 */
-
+    printf("[error]: Hard fault. Please reset the device.");
     /* USER CODE END HardFault_IRQn 0 */
     while (1) {
         /* USER CODE BEGIN W1_HardFault_IRQn 0 */
