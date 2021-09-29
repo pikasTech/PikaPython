cca017bd04d7ab783cf545bf8b2af0eb23e121ef
graduateDesign <graduateDesign@example.com>
2021-09-29 22:10:57 +0800

    new obj by script fit for g0

--- demo/stm32g030c8/pikascript/main.py
+++ demo/stm32g030c8/pikascript/main.py
@@ -11,3 +11,15 @@ pwm = STM32.PWM()
 rgb = PikaPiZero.RGB()
 mem = PikaStdLib.MemChecker()
 
+rgb.init()
+rgb.enable()
+
+print('hello 2')
+print('mem used max:')
+mem.max()
+
+while True:
+    time.sleep_ms(10)
+    rgb.flow()
+    print('flowing...')
+
