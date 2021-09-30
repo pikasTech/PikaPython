749955d42a660f39994bb155a8d7c68e431eb0c9
lyon <catgit.@catgit.com>
2021-10-01 00:23:38 +0800

    rm old file

--- demo/stm32f103zet6/demo02-led-stm32f103zet6_tworoot/mimiScript/Inc/sysObj.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MimiObj_sys__H
-#define _MimiObj_sys__H
-#include "MimiObj.h"
-
-MimiObj *New_MimiObj_sys(Args *args);
-#endif
--- demo/stm32f407-Arm-2D/stm32f407-Arm-2D-boxes/pikascript/pikascript-core/TinyObj.h
+++
@@ -1,5 +0,0 @@
-#ifndef __TYNYOBJ__H
-#define __TYNYOBJ__H
-#include "MimiObj.h"
-MimiObj *New_TinyObj(Args *args);
-#endif
