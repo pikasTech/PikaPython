3719dc756609133fb8eb553024b29581d8407d54
graduateDesign <graduateDesign@example.com>
2021-10-01 22:23:20 +0800

    splidt releases ok

--- pikaPackageManager/main.go
+++ pikaPackageManager/main.go
@@ -12,6 +12,25 @@ import (
 	"github.com/go-git/go-git/v5"
 )
 
+func main() {
+	superPath := "/tmp"
+	path := "/pikascript"
+
+	go readPathSize(superPath + path)
+	updatePikascript(superPath + path)
+
+	_, _ = getPackages(superPath + path)
+	fmt.Printf("\n")
+	_, _ = getRequestment("requestment.txt")
+	fmt.Printf("\n")
+
+	fmt.Println("update OK !")
+	for i := 3; i >= 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Println("this window will auto close after", i, "s...")
+	}
+}
+
 var isShowSize = false
 
 type Requerment_t struct {
@@ -20,31 +39,14 @@ type Requerment_t struct {
 }
 
 type Package_t struct {
-	Name     string
-	Releases []string
+	Name           string
+	Releases       []string
+	ReleasesName   []string
+	ReleasesCommit []string
 }
 
 type Config_t struct {
 	Packages []Package_t
-}
-
-func main() {
-	superPath := "/tmp"
-	path := "/pikascript"
-
-	go readPathSize(superPath + path)
-	updatePikascript(superPath + path)
-
-	getPackages(superPath + path)
-	fmt.Printf("\n")
-	getRequestment("requestment.txt")
-	fmt.Printf("\n")
-
-	fmt.Println("update OK !")
-	for i := 3; i >= 0; i-- {
-		time.Sleep(1 * time.Second)
-		fmt.Println("this window will auto close after", i, "s...")
-	}
 }
 
 func getRequestment(path string) ([]Requerment_t, bool) {
@@ -65,19 +67,23 @@ func getRequestment(path string) ([]Requerment_t, bool) {
 	return requestments, true
 }
 
-func getPackages(path string) (Config_t, bool) {
+func getPackages(path string) ([]Package_t, bool) {
 	var config Config_t
 	if _, err := toml.DecodeFile(path+"/pikaPackageManager/packages.toml", &config); err != nil {
 		fmt.Println(err)
-		return config, false
+		return config.Packages, false
 	}
 	for _, pkg := range config.Packages {
 		fmt.Printf("found package: %s\n", pkg.Name)
+		i := 0
 		for _, release := range pkg.Releases {
-			fmt.Printf("    release: %s\n", release)
+			pkg.ReleasesName = append(pkg.ReleasesName, strings.Split(release, " ")[0])
+			pkg.ReleasesCommit = append(pkg.ReleasesCommit, strings.Split(release, " ")[1])
+			fmt.Printf("    release: %s %s\n", pkg.ReleasesName[i], pkg.ReleasesCommit[i])
+			i++
 		}
 	}
-	return config, true
+	return config.Packages, true
 }
 
 func readPathSize(path string) {
