91d0c33dfd0a0105bb73f12baa0f2bf08becade6
graduateDesign <graduateDesign@example.com>
2021-10-04 01:08:08 +0800

    change example to bsp

---
+++ bsp/stm32g030c8/pikascript/main.py
@@ -0,0 +1,30 @@
+import STM32
+import PikaPiZero
+import PikaStdLib
+
+time = STM32.Time()
+uart = STM32.UART()
+adc = STM32.ADC()
+pin = STM32.GPIO()
+pwm = STM32.PWM()
+uart = STM32.UART()
+rgb = PikaPiZero.RGB()
+mem = PikaStdLib.MemChecker()
+
+uart.init()
+uart.setId(1)
+uart.setBaudRate(115200)
+uart.enable()
+
+rgb.init()
+rgb.enable()
+
+print('hello 2')
+print('mem used max:')
+mem.max()
+
+while True:
+    time.sleep_ms(10)
+    rgb.flow()
+    print('flowing...')
+
