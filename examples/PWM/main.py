86a4b0ceeef265afe3b0d2cca021236ffd02f46d
Lyon <liang6516@outlook.com>
2021-10-04 10:04:06 +0800

    add examples

---
+++ examples/PWM/main.py
@@ -0,0 +1,15 @@
+import PikaStdLib
+import STM32
+
+time = STM32.Time()
+pwm = STM32.PWM()
+pwm.setPin('PA8')
+pwm.setFrequency(2000)
+pwm.setDuty(0.5)
+pwm.enable()
+
+while True:
+    time.sleep_ms(500)
+    pwm.setDuty(0.5)
+    time.sleep_ms(500)
+    pwm.setDuty(0.3)
