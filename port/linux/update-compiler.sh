073357ef35dd9f8117fbea207eb1efee5f7c460a
Lyon <liang6516@outlook.com>
2021-10-04 09:42:46 +0800

    update path for core

--- port/linux/update-compiler.sh
+++ port/linux/update-compiler.sh
@@ -1,6 +1,6 @@
 ROOT=$PWD
-cd ../pikaCompiler
+cd ../tools/pikaCompiler
 cargo build
 cd $ROOT
-cp ../pikaCompilertarget/debug/rust-msc package/pikascript/rust-msc-latest-linux
-cp ../pikaCompilertarget/debug/rust-msc.exe package/pikascript/rust-msc-latest-win10.exe
+cp ../tools/pikaCompilertarget/debug/rust-msc package/pikascript/rust-msc-latest-linux
+cp ../tools/pikaCompilertarget/debug/rust-msc.exe package/pikascript/rust-msc-latest-win10.exe
