a53e9165068e9fe8d0ebfa6e0bc5566a7de32753
Meco Man <920369182@qq.com>
2021-10-05 00:16:11 -0400

    增加PIKA_WEAK宏用于适应不同编译链;增加pika_free/malloc/printf

--- src/dataMemory.c
+++ src/dataMemory.c
@@ -9,19 +9,29 @@
 
 PikaMemInfo pikaMemInfo = {0};
 
-__attribute__((weak)) void __pikaDisableIrqHandle(){
+PIKA_WEAK void __pikaDisableIrqHandle(){
     /* disable irq to support thread */
 }
-__attribute__((weak)) void __pikaEnableIrqHandle(){
+
+PIKA_WEAK void __pikaEnableIrqHandle(){
     /* disable irq to support thread */    
 }
+
+PIKA_WEAK void *pika_malloc(size_t size){
+    return malloc(size);
+}
+
+PIKA_WEAK void pika_free(void *ptr){
+    return free(ptr);
+}
+
 void* pikaMalloc(uint32_t size) {
     pikaMemInfo.heapUsed += size;
     if (pikaMemInfo.heapUsedMax < pikaMemInfo.heapUsed) {
         pikaMemInfo.heapUsedMax = pikaMemInfo.heapUsed;
     }
     __pikaDisableIrqHandle();
-    void* mem = malloc(size);
+    void* mem = pika_malloc(size);
     __pikaEnableIrqHandle();
     if (NULL == mem) {
         printf("[error]: No heap space! Please reset the device.\r\n");
@@ -33,7 +43,7 @@ void* pikaMalloc(uint32_t size) {
 
 void pikaFree(void* mem, uint32_t size) {
     __pikaDisableIrqHandle();
-    free(mem);
+    pika_free(mem);
     __pikaEnableIrqHandle();    
     pikaMemInfo.heapUsed -= size;
 }
