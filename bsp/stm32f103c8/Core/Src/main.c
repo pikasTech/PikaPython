22b445a2c80eb6394b5dccdeec7de135d6cefb33
graduateDesign <graduateDesign@example.com>
2021-10-09 00:29:22 +0800

    build from keil is ok

--- bsp/stm32f103c8/Core/Src/main.c
+++ bsp/stm32f103c8/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "pikaScript.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
