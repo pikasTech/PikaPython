286eb4286d5a250e70d42802706b67e6a55a487a
graduateDesign <graduateDesign@example.com>
2021-10-09 00:34:35 +0800

    add module in config.h

--- bsp/stm32f103c8/Core/Inc/stm32f1xx_hal_conf.h
+++ bsp/stm32f103c8/Core/Inc/stm32f1xx_hal_conf.h
@@ -75,6 +75,9 @@
 #define HAL_GPIO_MODULE_ENABLED
 #define HAL_PWR_MODULE_ENABLED
 #define HAL_RCC_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
+#define HAL_TIM_MODULE_ENABLED
+#define HAL_ADC_MODULE_ENABLED
 
 /* ########################## Oscillator Values adaptation ####################*/
 /**
