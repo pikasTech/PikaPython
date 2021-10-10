cd7e342c698af988ad86a700ef2cad61357acbce
lyon1998 <645275593@qq.com>
2021-10-11 00:11:35 +0800

    adding math class to pika std lib

--- port/linux/package/pikascript/PikaStdLib.py
+++ port/linux/package/pikascript/PikaStdLib.py
@@ -1,6 +1,38 @@
 from PikaObj import *
 
 
+class Math(TinyObj):
+    def plusInt(num1: int, num2: int) -> int:
+        pass
+
+    def plusFloat(num1: float, num2: float) -> float:
+        pass
+
+    def minusInt(num1: int, num2: int) -> int:
+        pass
+
+    def minusFloat(num1: float, num2: float) -> float:
+        pass
+
+    def equalInt(num1: int, num2: int) -> int:
+        pass
+
+    def equalFloat(num1: float, num2: float) -> int:
+        pass
+
+    def graterThanInt(num1: int, num2: int) -> int:
+        pass
+
+    def lessThanInt(num1: int, num2: int) -> int:
+        pass
+
+    def graterThanFloat(num1: float, num2: float) -> int:
+        pass
+
+    def lessThanFloat(num1: float, num2: float) -> int:
+        pass
+
+
 class MemChecker(BaseObj):
     def max():
         pass
@@ -13,7 +45,6 @@ class MemChecker(BaseObj):
 
 
 class SysObj(BaseObj):
-
     def type(argPath: str):
         pass
 
--- port/linux/package/pikascript/main.py
+++ port/linux/package/pikascript/main.py
@@ -2,6 +2,8 @@ import PikaStdLib
 import PikaStdDevice
 from PikaObj import *
 
+meth = PikaStdLib.Math()
+
 print('hello world')
 mem = PikaStdLib.MemChecker()
 print('mem.max :')
