6db53ff29f6be7a5ac11a5160cb1513b6d77f1f5
lyon1998 <645275593@qq.com>
2021-10-11 19:41:50 +0800

    add pikaMath pacakge to support base operator

---
+++ package/PikaMath/PikaMath.py
@@ -0,0 +1,41 @@
+from PikaObj import *
+
+class Operator(TinyObj):
+    def plusInt(num1: int, num2: int) -> int:
+        pass
+
+    def plusFloat(num1: float, num2: float) -> float:
+        pass
+
+    def minusInt(num1: int, num2: int) -> int:
+        pass
+
+    def minusFloat(num1: float, num2: float) -> float:
+        pass
+
+    def equalInt(num1: int, num2: int) -> int:
+        pass
+
+    def equalFloat(num1: float, num2: float) -> int:
+        pass
+
+    def graterThanInt(num1: int, num2: int) -> int:
+        pass
+
+    def graterThanFloat(num1: float, num2: float) -> int:
+        pass
+
+    def lessThanInt(num1: int, num2: int) -> int:
+        pass
+
+    def lessThanFloat(num1: float, num2: float) -> int:
+        pass
+
+    def AND(flag1: int, flag2: int) -> int:
+        pass
+
+    def OR(flag1: int, flag2: int) -> int:
+        pass
+
+    def NOT(flag: int) -> int:
+        pass
