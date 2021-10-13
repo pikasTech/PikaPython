3e4da111e0c068b06221e4446d793f7d3108b47a
lyon1998 <645275593@qq.com>
2021-10-13 19:41:04 +0800

    use parse_STmt

--- src/PikaParser.c
+++ src/PikaParser.c
@@ -3,20 +3,31 @@
 #include "dataQueueObj.h"
 #include "dataStrs.h"
 
-AST* pikaParse(char* line) {
-    AST* ast = New_queueObj();
+AST* AST_parseStmt(AST* ast, char* stmt) {
     Args* buffs = New_strBuff();
-    char* stmt = strsGetCleanCmd(buffs, line);
     char* assignment = strsGetFirstToken(buffs, stmt, '(');
     char* direct = NULL;
     char* method = NULL;
     if (strIsContain(assignment, '=')) {
         direct = strsGetFirstToken(buffs, assignment, '=');
+        obj_setStr(ast, "direct", direct);
         method = strsGetLastToken(buffs, assignment, '=');
     }
     method = assignment;
+    obj_setStr(ast, "method", method);
     char* subStmts = strsCut(buffs, stmt, '(', ')');
 
+    goto exit;
+exit:
+    args_deinit(buffs);
+    return ast;
+}
+
+AST* pikaParse(char* line) {
+    AST* ast = New_queueObj();
+    Args* buffs = New_strBuff();
+    char* stmt = strsGetCleanCmd(buffs, stmt);
+    ast = AST_parseStmt(ast, stmt);
     goto exit;
 exit:
     args_deinit(buffs);
