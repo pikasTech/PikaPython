99ca8d30c3366ae2bf817ff793ae93c2d67bc0de
lyon1998 <645275593@qq.com>
2021-10-14 15:20:13 +0800

    add PikaVM

---
+++ src/PikaVM.c
@@ -0,0 +1,9 @@
+#include "PikaVM.h"
+#include "PikaObj.h"
+#include "PikaParser.h"
+#include "dataQueueObj.h"
+#include "dataStrs.h"
+
+int32_t pikaVM_run(PikaObj* self, char* pikaAsm, int32_t LineNumber) {
+    return LineNumber;
+}
---
+++ src/PikaVM.h
@@ -0,0 +1,7 @@
+#ifndef __PIKA__VM__H
+#define __PIKA__VM__H
+#include "PikaObj.h"
+
+int32_t pikaVM_run(PikaObj* self, char* pikaAsm, int32_t lineNumber);
+
+#endif
