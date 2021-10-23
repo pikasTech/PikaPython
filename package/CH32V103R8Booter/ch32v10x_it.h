b92ef8c347860077d259e8f4504b3ed8e55da85d
lyon1998 <645275593@qq.com>
2021-10-23 21:52:22 +0800

    add ch32 booter

---
+++ package/CH32V103R8Booter/ch32v10x_it.h
@@ -0,0 +1,16 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : ch32v10x_it.h
+* Author             : WCH
+* Version            : V1.0.0
+* Date               : 2020/04/30
+* Description        : This file contains the headers of the interrupt handlers.
+*******************************************************************************/
+#ifndef __CH32V10x_IT_H
+#define __CH32V10x_IT_H
+
+#include "debug.h"
+
+
+#endif /* __CH32V10x_IT_H */
+
+
