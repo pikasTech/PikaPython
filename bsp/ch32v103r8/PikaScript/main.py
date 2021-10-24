2c97176e419ef778e7ae90b537ae56fb0b42a7e9
lyon1998 <645275593@qq.com>
2021-10-24 21:32:23 +0800

    add ch32 packege for ch32v103 bsp

--- bsp/ch32v103r8/PikaScript/main.py
+++ bsp/ch32v103r8/PikaScript/main.py
@@ -1,8 +1,15 @@
 import PikaStdLib
+import CH32V103
 
 mem = PikaStdLib.MemChecker()
 print('hello PikaScript !')
 
+io1 = CH32V103.GPIO()
+io1.init()
+io1.setPin('PA1')
+io1.setMode('out')
+io1.enable()
+io1.high()
 
 print(1.4)
 print('mem used max:')
