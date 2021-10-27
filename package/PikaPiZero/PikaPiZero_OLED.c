23ce6a149899a17ddad87e0ef51b951ff02dabd3
李昂 <liang6516@outlook.com>
2021-10-27 10:33:42 +0800

    add OLED c lib

---
+++ package/PikaPiZero/PikaPiZero_OLED.c
@@ -0,0 +1,18 @@
+#include "PikaPiZero_OLED.h"
+#include "oled.h"
+
+void PikaPiZero_OLED_drawPoint(PikaObj *self, int x, int y){
+    OLED_DrawPoint(x, y, 1);
+    OLED_Refresh();    
+}
+
+void PikaPiZero_OLED_init(PikaObj *self){
+    OLED_Init();
+	OLED_ColorTurn(0);
+    OLED_DisplayTurn(0);
+    OLED_Clear();
+}
+
+void PikaPiZero_OLED_clear(PikaObj *self){
+    OLED_Clear();
+}
