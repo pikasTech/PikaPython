df940d35eef06c94e9dd8151a4bb3793380de7f9
李昂 <liang6516@outlook.com>
2021-11-01 19:25:57 +0800

    debug arm2d pacakge

--- package/Arm2D/Arm2D_Screen.c
+++ package/Arm2D/Arm2D_Screen.c
@@ -2,35 +2,35 @@
 
 void Arm2D_Screen_newBox(PikaObj *self, char *name)
 {
-    Args *buffs = New_strBuff();
-    PikaObj *pikaMain = obj_getPtr(self, "__context");
-    int res = obj_newObj(pikaMain, strsFormat(buffs, "screen.elems.%s", name), "Arm2D_Box");
-    {
-        if (res == 1)
-        {
-            obj_setErrorCode(self, 1);
-            obj_setSysOut(self, "[error]: new Box faild! class no found!");
-            goto exit;
-        }
-        if (res == 2)
-        {
-            obj_setErrorCode(self, 2);
-            obj_setSysOut(self, "[error]: new Box faild! object host no found!");
-            goto exit;
-        }
-    }
-    obj_run(pikaMain, strsFormat(buffs, "screen.elems.%s.init()", name));
-exit:
-    args_deinit(buffs);
+//    Args *buffs = New_strBuff();
+//    PikaObj *pikaMain = obj_getPtr(self, "__context");
+//    int res = obj_newObj(pikaMain, strsFormat(buffs, "screen.elems.%s", name), "Arm2D_Box");
+//    {
+//        if (res == 1)
+//        {
+//            obj_setErrorCode(self, 1);
+//            obj_setSysOut(self, "[error]: new Box faild! class no found!");
+//            goto exit;
+//        }
+//        if (res == 2)
+//        {
+//            obj_setErrorCode(self, 2);
+//            obj_setSysOut(self, "[error]: new Box faild! object host no found!");
+//            goto exit;
+//        }
+//    }
+//    obj_run(pikaMain, strsFormat(buffs, "screen.elems.%s.init()", name));
+//exit:
+//    args_deinit(buffs);
 }
 
 void Arm2D_Screen_newStar(PikaObj *self, char *name)
 {
-    Args *buffs = New_strBuff();
-    PikaObj *pikaMain = obj_getPtr(self, "__context");
-    obj_newObj(pikaMain, strsFormat(buffs, "screen.elems.%s", name), "Arm2D_Star");
-    obj_run(pikaMain, strsFormat(buffs, "screen.elems.%s.init()", name));
-    args_deinit(buffs);
+//    Args *buffs = New_strBuff();
+//    PikaObj *pikaMain = obj_getPtr(self, "__context");
+//    obj_newObj(pikaMain, strsFormat(buffs, "screen.elems.%s", name), "Arm2D_Star");
+//    obj_run(pikaMain, strsFormat(buffs, "screen.elems.%s.init()", name));
+//    args_deinit(buffs);
 }
 
 void Arm2D_Screen_init(PikaObj *self)
--- package/Arm2D/Arm2D_star.c
+++ package/Arm2D/Arm2D_star.c
@@ -23,57 +23,57 @@ void Arm2D_Star_speed(PikaObj *self, float speed)
 
 void Arm2D_Star_init(PikaObj *self)
 {
-    obj_setInt(self, "posX", 250);
-    obj_setInt(self, "posY", 250);
-    obj_setInt(self, "centreX", 0);
-    obj_setInt(self, "centreY", 0);
-    obj_setFloat(self, "angleSpeed", 0.5f);
-    obj_setFloat(self, "angle", 0.0f);
-
-    rotate_tile_t rotateTile = {
-        .ptTile = &c_tPictureSun,
-        .fAngleSpeed = 0.0f,
-        .tCentre = {
-            .iX = 0,
-            .iY = 0,
-        },
-        .tRegion = {
-            .tLocation = {
-                .iX = 200,
-                .iY = 200,
-            },
-            .tSize = c_tPictureSun.tRegion.tSize,
-        },
-        .fAngle = 0.0,
-    };
-
-    Arg *rotateTileArg = New_arg(NULL);
-    arg_setcontent(rotateTileArg, &rotateTile, sizeof(rotate_tile_t));
-    arg_setName(rotateTileArg, "rotateTile");
-    arg_setType(rotateTileArg, "rotate_tile_t");
-    obj_setArg(self, "rotateTile", rotateTileArg);
-    arg_deinit(rotateTileArg);
+//    obj_setInt(self, "posX", 250);
+//    obj_setInt(self, "posY", 250);
+//    obj_setInt(self, "centreX", 0);
+//    obj_setInt(self, "centreY", 0);
+//    obj_setFloat(self, "angleSpeed", 0.5f);
+//    obj_setFloat(self, "angle", 0.0f);
+//
+//    rotate_tile_t rotateTile = {
+//        .ptTile = &c_tPictureSun,
+//        .fAngleSpeed = 0.0f,
+//        .tCentre = {
+//            .iX = 0,
+//            .iY = 0,
+//        },
+//        .tRegion = {
+//            .tLocation = {
+//                .iX = 200,
+//                .iY = 200,
+//            },
+//            .tSize = c_tPictureSun.tRegion.tSize,
+//        },
+//        .fAngle = 0.0,
+//    };
+//
+//    Arg *rotateTileArg = New_arg(NULL);
+//    arg_setcontent(rotateTileArg, &rotateTile, sizeof(rotate_tile_t));
+//    arg_setName(rotateTileArg, "rotateTile");
+//    arg_setType(rotateTileArg, "rotate_tile_t");
+//    obj_setArg(self, "rotateTile", rotateTileArg);
+//    arg_deinit(rotateTileArg);
 }
 
 void Arm2D_Star_update(PikaObj *self, int bIsNewFrame, void *ptTile)
 {
-    Arg *rotateTileArg = obj_getArg(self, "rotateTile");
-    rotate_tile_t *s_tSun = arg_getcontent(rotateTileArg);
-    s_tSun->tRegion.tLocation.iX = obj_getInt(self, "posX");
-    s_tSun->tRegion.tLocation.iY = obj_getInt(self, "posY");
-    s_tSun->tCentre.iX = obj_getInt(self, "centreX");
-    s_tSun->tCentre.iY = obj_getInt(self, "centreY");
-    s_tSun->fAngleSpeed = obj_getFloat(self, "angleSpeed");
-
-    if (bIsNewFrame)
-    {
-        s_tSun->fAngle += s_tSun->fAngleSpeed;
-    }
-    arm_2dp_rgb565_tile_rotation(&(s_tSun->tOP),
-                                 s_tSun->ptTile,
-                                 ptTile,
-                                 &(s_tSun->tRegion),
-                                 s_tSun->tCentre,
-                                 s_tSun->fAngle,
-                                 GLCD_COLOR_WHITE);
+//    Arg *rotateTileArg = obj_getArg(self, "rotateTile");
+//    rotate_tile_t *s_tSun = arg_getcontent(rotateTileArg);
+//    s_tSun->tRegion.tLocation.iX = obj_getInt(self, "posX");
+//    s_tSun->tRegion.tLocation.iY = obj_getInt(self, "posY");
+//    s_tSun->tCentre.iX = obj_getInt(self, "centreX");
+//    s_tSun->tCentre.iY = obj_getInt(self, "centreY");
+//    s_tSun->fAngleSpeed = obj_getFloat(self, "angleSpeed");
+//
+//    if (bIsNewFrame)
+//    {
+//        s_tSun->fAngle += s_tSun->fAngleSpeed;
+//    }
+//    arm_2dp_rgb565_tile_rotation(&(s_tSun->tOP),
+//                                 s_tSun->ptTile,
+//                                 ptTile,
+//                                 &(s_tSun->tRegion),
+//                                 s_tSun->tCentre,
+//                                 s_tSun->fAngle,
+//                                 GLCD_COLOR_WHITE);
 }
