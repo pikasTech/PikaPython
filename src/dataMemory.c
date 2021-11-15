c374ce882a92862b2556c7c195807d18d89827f2
lyon <liang6516@outlook.com>
2021-11-15 22:52:30 +0800

    move pika_pool to config

--- src/dataMemory.c
+++ src/dataMemory.c
@@ -7,23 +7,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define pika_aline 8
-#define pika_pool_size 8192
-uint8_t pika_bitmap[pika_pool_size / pika_aline / 8] = {0};
-uint8_t pika_pool_mem[pika_pool_size] = {0};
-PikaMemInfo pikaMemInfo = {0};
-Pool pikaPool = {.aline = pika_aline,
-                 .bitmap = pika_bitmap,
-                 .mem = pika_pool_mem,
-                 .size = pika_pool_size};
 
-void* __impl_pikaMalloc(size_t size) {
-    void* mem = pool_malloc(&pikaPool, size);
-    return mem;
-}
-void __impl_pikaFree(void* ptrm, size_t size) {
-    pool_free(&pikaPool, ptrm, size);
-}
+PikaMemInfo pikaMemInfo = {0};
 
 void* pikaMalloc(uint32_t size) {
     pikaMemInfo.heapUsed += size;
