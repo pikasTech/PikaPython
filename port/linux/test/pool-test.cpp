0f137eb528c3c5378d83398fd841a8d4183965e5
lyon <liang6516@outlook.com>
2021-11-15 20:22:26 +0800

    add pool-test

---
+++ port/linux/test/pool-test.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+extern "C" {
+#include "dataArg.h"
+#include "dataMemory.h"
+#include "dataString.h"
+}
+
+TEST(pool, init) {
+    Pool p = pool_init(125, 4);
+    EXPECT_EQ(p.aline, 4);
+    EXPECT_EQ(p.size, 128);
+    pool_deinit(&p);
+    EXPECT_EQ((uint64_t)p.mem, NULL);
+}
