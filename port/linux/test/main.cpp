f351299a93acbaad79f130610b8d4f8a2513a28a
lyon <liang6516@outlook.com>
2021-11-16 16:25:22 +0800

    add dynamic pool in mian.cpp for test

--- port/linux/test/main.cpp
+++ port/linux/test/main.cpp
@@ -1,6 +1,26 @@
 #include "gtest/gtest.h"
 
+extern "C" {
+#include "dataMemory.h"
+}
+
+#define use_dynamic_pool 1
+/* use mem pool */
+#if use_dynamic_pool
+Pool pikaPool;
+void* __impl_pikaMalloc(size_t size) {
+    void* mem = pool_malloc(&pikaPool, size);
+    return mem;
+}
+void __impl_pikaFree(void* ptrm, size_t size) {
+    pool_free(&pikaPool, ptrm, size);
+}
+#endif
+
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+#if use_dynamic_pool
+    pikaPool = pool_init(0x1B00, 4);
+#endif
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
--- port/linux/test/mem_pool_config.c
+++ port/linux/test/mem_pool_config.c
@@ -1,15 +1,16 @@
 #include "dataMemory.h"
 
-#define use_mem_pool 1
-#define pika_aline 8
-#define pika_pool_size 8192
+#define use_const_pool 0
+
+#if use_const_pool
+#define pika_aline 4
+#define pika_pool_size 0x1B00
 uint8_t pika_bitmap[pika_pool_size / pika_aline / 8] = {0};
 uint8_t pika_pool_mem[pika_pool_size] = {0};
 Pool pikaPool = {.aline = pika_aline,
                  .bitmap = pika_bitmap,
                  .mem = pika_pool_mem,
                  .size = pika_pool_size};
-#if use_mem_pool
 void* __impl_pikaMalloc(size_t size) {
     void* mem = pool_malloc(&pikaPool, size);
     return mem;
@@ -17,4 +18,5 @@ void* __impl_pikaMalloc(size_t size) {
 void __impl_pikaFree(void* ptrm, size_t size) {
     pool_free(&pikaPool, ptrm, size);
 }
+
 #endif
