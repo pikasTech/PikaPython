6480ec1adb0d7272de0fe8b9d0be47dc24cfca9a
lyon <liang6516@outlook.com>
2021-11-17 10:10:18 +0800

    add pikastd_list.c

---
+++ port/linux/package/pikascript/pikascript-lib/PikaStdLib/PikaStdLib.py
@@ -0,0 +1,20 @@
+from PikaObj import *
+
+
+class MemChecker(TinyObj):
+    def max():
+        pass
+
+    def now():
+        pass
+
+    def resetMax():
+        pass
+
+
+class SysObj(BaseObj):
+    def type(argPath: str):
+        pass
+
+    def remove(argPath: str):
+        pass
---
+++ port/linux/package/pikascript/pikascript-lib/PikaStdLib/PikaStdLib_List.c
@@ -0,0 +1,11 @@
+#include "PikaStdLib_List.h"
+
+void PikaStdLib_List_append(PikaObj* self, Arg* arg) {}
+int PikaStdLib_List_len(PikaObj* self) {
+    return 0;
+}
+Arg* PikaStdLib_List_get(PikaObj* self, int i) {
+    return NULL;
+}
+void PikaStdLib_List_remove(PikaObj* self, Arg* arg) {}
+void PikaStdLib_List_init(PikaObj* self) {}
