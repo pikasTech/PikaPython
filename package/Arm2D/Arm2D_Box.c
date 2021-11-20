c584cc1593eb0b4cc8f0851c538153a0ae0ad286
pikastech <liang6516@outlook.com>
2021-11-20 20:23:11 +0800

    update for arm2d in g030

--- package/Arm2D/Arm2D_Box.c
+++ package/Arm2D/Arm2D_Box.c
@@ -9,67 +9,87 @@ void Arm2D_Box_init(PikaObj* self) {
     elemInfo.alpha = 255;
     elemInfo.x = 0;
     elemInfo.y = 0;
-    args_setStruct(self, "elemInfo", &elemInfo, sizeof(pika_arm2d_element_info_t));
+    args_setStruct(self->attributeList, "elemInfo", &elemInfo,
+                   sizeof(pika_arm2d_element_info_t));
     /* init box info */
     pika_arm2d_box_info_t boxInfo = {0};
     boxInfo.wight = 50;
     boxInfo.hight = 50;
     boxInfo.color_code = getColorCode("blue");
     /* load box info to arg */
-    args_setStruct(self, "boxInfo", &boxInfo, sizeof(pika_arm2d_box_info_t));
+    args_setStruct(self->attributeList, "boxInfo", &boxInfo,
+                   sizeof(pika_arm2d_box_info_t));
     /* add dirtyReginItem */
-    args_setStruct(self, "dirtyReginItem", NULL, sizeof(arm_2d_region_list_item_t));
+    args_setStruct(self->attributeList, "dirtyReginItem", NULL,
+                   sizeof(arm_2d_region_list_item_t));
 }
 
-int __min(int x,int y){
-	return x<y?x:y;
+int __min(int x, int y) {
+    return x < y ? x : y;
 }
 
-int __max(int x,int y){
-	return x>y?x:y;
+int __max(int x, int y) {
+    return x > y ? x : y;
 }
 
 void Arm2D_Box_update(PikaObj* self) {
-    pika_arm2d_box_info_t* boxInfo = args_getStruct(self, "boxInfo");
-
+    pika_arm2d_box_info_t boxInfo;
+    memcpy(&boxInfo, args_getStruct(self->attributeList, "boxInfo"),
+           sizeof(pika_arm2d_box_info_t));
     void* target_tile = pika_arm2d_window.pfb_tile_now;
     bool bIsNewFrame = pika_arm2d_window.pfb_is_new_frame;
-    if(bIsNewFrame){
-        pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
 
-        boxInfo->arg2d_regin.tSize.iHeight = boxInfo->hight;
-        boxInfo->arg2d_regin.tSize.iWidth = boxInfo->wight;
-        boxInfo->arg2d_regin.tLocation.iX = elemInfo->x;
-        boxInfo->arg2d_regin.tLocation.iY = elemInfo->y;
-        memcpy(&(boxInfo->elem_info), elemInfo, sizeof(pika_arm2d_element_info_t));
+    if (bIsNewFrame) {
+        pika_arm2d_element_info_t elemInfo;
+        memcpy(&elemInfo, args_getStruct(self->attributeList, "elemInfo"),
+               sizeof(pika_arm2d_element_info_t));
 
-        arm_2d_region_list_item_t* dirtyReginItem = args_getStruct(self, "dirtyReginItem");
-        dirtyReginItem->ptNext = NULL;
-        int x_start = __min(boxInfo->elem_info.x, boxInfo->elem_info.x_last) - 14;
-        int y_start = __min(boxInfo->elem_info.y, boxInfo->elem_info.y_last) - 7;
-        int x_end = __max(boxInfo->elem_info.x + boxInfo->wight, boxInfo->elem_info.x_last + boxInfo->wight_last);
-        int y_end = __max(boxInfo->elem_info.y + boxInfo->hight, boxInfo->elem_info.y_last + boxInfo->hight_last);
-        int hight = y_end - y_start + 7;
-        int wight = x_end - x_start + 14;
-        dirtyReginItem->tRegion.tLocation.iX = x_start;
-        dirtyReginItem->tRegion.tLocation.iY = y_start;
-        dirtyReginItem->tRegion.tSize.iHeight = hight;
-        dirtyReginItem->tRegion.tSize.iWidth = wight;
-        pika_arm2d_window.dirty_region_list = dirtyReginItem;
+        boxInfo.arg2d_regin.tSize.iHeight = boxInfo.hight;
+        boxInfo.arg2d_regin.tSize.iWidth = boxInfo.wight;
+        boxInfo.arg2d_regin.tLocation.iX = elemInfo.x;
+        boxInfo.arg2d_regin.tLocation.iY = elemInfo.y;
+
+        memcpy(&(boxInfo.elem_info), &elemInfo,
+               sizeof(pika_arm2d_element_info_t));
+
+        args_setStruct(self->attributeList, "boxInfo", &boxInfo,
+                       sizeof(pika_arm2d_box_info_t));
+
+        arm_2d_region_list_item_t dirtyReginItem;
+        memcpy(&dirtyReginItem,
+               args_getStruct(self->attributeList, "dirtyReginItem"),
+               sizeof(arm_2d_region_list_item_t));
+//        dirtyReginItem.ptNext = NULL;
+//        int x_start = __min(boxInfo.elem_info.x, boxInfo.elem_info.x_last) - 14;
+//        int y_start = __min(boxInfo.elem_info.y, boxInfo.elem_info.y_last) - 7;
+//        int x_end = __max(boxInfo.elem_info.x + boxInfo.wight,
+//                          boxInfo.elem_info.x_last + boxInfo.wight_last);
+//        int y_end = __max(boxInfo.elem_info.y + boxInfo.hight,
+//                          boxInfo.elem_info.y_last + boxInfo.hight_last);
+//        int hight = y_end - y_start + 7;
+//        int wight = x_end - x_start + 14;
+//        dirtyReginItem.tRegion.tLocation.iX = x_start;
+//        dirtyReginItem.tRegion.tLocation.iY = y_start;
+//        dirtyReginItem.tRegion.tSize.iHeight = hight;
+//        dirtyReginItem.tRegion.tSize.iWidth = wight;
+//        pika_arm2d_window.dirty_region_list = &dirtyReginItem;
     }
-    draw_round_corner_box(target_tile, &(boxInfo->arg2d_regin), boxInfo->color_code,
-    		boxInfo->elem_info.alpha, bIsNewFrame);
+    arm_2d_rgb16_fill_colour(target_tile, &boxInfo.arg2d_regin, boxInfo.color_code);
+    //draw_round_corner_box(target_tile, &boxInfo.arg2d_regin, boxInfo.color_code,
+    //                      boxInfo.elem_info.alpha, bIsNewFrame);
 }
 
 void Arm2D_Box_setColor(PikaObj* self, char* color) {
-    pika_arm2d_box_info_t* boxInfo = args_getStruct(self, "boxInfo");
+    pika_arm2d_box_info_t* boxInfo =
+        args_getStruct(self->attributeList, "boxInfo");
     boxInfo->color_code_last = boxInfo->color_code;
 
     boxInfo->color_code = getColorCode(color);
 }
 
 void Arm2D_Box_setSize(PikaObj* self, int x, int y) {
-    pika_arm2d_box_info_t* boxInfo = args_getStruct(self, "boxInfo");
+    pika_arm2d_box_info_t* boxInfo =
+        args_getStruct(self->attributeList, "boxInfo");
     boxInfo->wight_last = boxInfo->wight;
     boxInfo->hight_last = boxInfo->hight;
 
--- package/Arm2D/Arm2D_Element.c
+++ package/Arm2D/Arm2D_Element.c
@@ -2,8 +2,8 @@
 #include "Arm2D_common.h"
 void Arm2D_Element_update(PikaObj* self) {
     /* need to be override */
-    args_setErrorCode(self, 1);
-    args_setSysOut(self, "[error]: update method not be overrided !");
+    args_setErrorCode(self->attributeList, 1);
+    args_setSysOut(self->attributeList, "[error]: update method not be overrided !");
 }
 
 void Arm2D_Element_init(PikaObj* self) {
@@ -11,49 +11,54 @@ void Arm2D_Element_init(PikaObj* self) {
     elemInfo.alpha = 255;
     elemInfo.x = 0;
     elemInfo.y = 0;
-    args_setStruct(self, "elemInfo", &elemInfo, sizeof(pika_arm2d_element_info_t));
+    args_setStruct(self->attributeList, "elemInfo", &elemInfo, sizeof(pika_arm2d_element_info_t));
 }
 
 void Arm2D_Element_setAlpha(PikaObj* self, int alpha) {
-    pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
+    pika_arm2d_element_info_t* elemInfo = args_getStruct(self->attributeList, "elemInfo");
     elemInfo->alpha_last = elemInfo->alpha;
 
     elemInfo->alpha = alpha;
 }
 
 void Arm2D_Element_up(PikaObj* self, int y) {
-    pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
+    pika_arm2d_element_info_t* elemInfo = args_getStruct(self->attributeList, "elemInfo");
     elemInfo->y_last = elemInfo->y;
 
     elemInfo->y = elemInfo->y - y;
 }
 
 void Arm2D_Element_down(PikaObj* self, int y) {
-    pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
+    pika_arm2d_element_info_t* elemInfo = args_getStruct(self->attributeList, "elemInfo");
     elemInfo->y_last = elemInfo->y;
 
     elemInfo->y = elemInfo->y + y;
 }
 
 void Arm2D_Element_lift(PikaObj* self, int x) {
-    pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
+    pika_arm2d_element_info_t* elemInfo = args_getStruct(self->attributeList, "elemInfo");
     elemInfo->x_last = elemInfo->x;
 
     elemInfo->x = elemInfo->x - x;
 }
 
 void Arm2D_Element_right(PikaObj* self, int x) {
-    pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
+    pika_arm2d_element_info_t* elemInfo = args_getStruct(self->attributeList, "elemInfo");
     elemInfo->x_last = elemInfo->x;
 
     elemInfo->x = elemInfo->x + x;
 }
 
 void Arm2D_Element_move(PikaObj* self, int x, int y) {
-    pika_arm2d_element_info_t* elemInfo = args_getStruct(self, "elemInfo");
-    elemInfo->y_last = elemInfo->y;
-    elemInfo->x_last = elemInfo->x;
+    pika_arm2d_element_info_t elemInfo;
+    memcpy(&elemInfo, args_getStruct(self->attributeList, "elemInfo"),
+    sizeof(pika_arm2d_element_info_t));
 
-    elemInfo->x = x;
-    elemInfo->y = y;
+    elemInfo.y_last = elemInfo.y;
+    elemInfo.x_last = elemInfo.x;
+
+    elemInfo.x = x;
+    elemInfo.y = y;
+    
+    args_setStruct(self->attributeList, "elemInfo", &elemInfo, sizeof(pika_arm2d_box_info_t));
 }
