d132f2c4c53834bc96261083e9b053b15ef2e242
lyon <liang6516@outlook.com>
2021-11-20 09:56:30 +0800

    move pool config together

--- port/linux/test/main.cpp
+++ port/linux/test/main.cpp
@@ -4,23 +4,8 @@ extern "C" {
 #include "dataMemory.h"
 }
 
-#define use_dynamic_pool 1
-/* use mem pool */
-#if use_dynamic_pool
-Pool pikaPool;
-void* __impl_pikaMalloc(size_t size) {
-    void* mem = pool_malloc(&pikaPool, size);
-    return mem;
-}
-void __impl_pikaFree(void* ptrm, size_t size) {
-    pool_free(&pikaPool, ptrm, size);
-}
-#endif
-
 int main(int argc, char** argv) {
-#if use_dynamic_pool
-    pikaPool = pool_init(1024 * 8, 8);
-#endif
+    __platformPoolInit();
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
--- port/linux/test/mem_pool_config.c
+++ port/linux/test/mem_pool_config.c
@@ -1,16 +1,17 @@
 #include "dataMemory.h"
 
 #define use_const_pool 0
+#define use_dynamic_pool 1
+
+#define pika_aline 8
+#define pika_pool_size 0x1B00
 
 /* quick_malloc is always open */
 uint8_t __is_quick_malloc(void) {
     // return 1;
     return 0;
 }
-
 #if use_const_pool
-#define pika_aline 4
-#define pika_pool_size 0x1B00
 uint8_t pika_bitmap[pika_pool_size / pika_aline / 8] = {0};
 uint8_t pika_pool_mem[pika_pool_size] = {0};
 Pool pikaPool = {.aline = pika_aline,
@@ -24,5 +25,18 @@ void* __impl_pikaMalloc(size_t size) {
 void __impl_pikaFree(void* ptrm, size_t size) {
     pool_free(&pikaPool, ptrm, size);
 }
+#endif
 
+#if use_dynamic_pool
+Pool pikaPool;
+void* __impl_pikaMalloc(size_t size) {
+    void* mem = pool_malloc(&pikaPool, size);
+    return mem;
+}
+void __impl_pikaFree(void* ptrm, size_t size) {
+    pool_free(&pikaPool, ptrm, size);
+}
+void __platformPoolInit(void) {
+    pikaPool = pool_init(pika_pool_size, pika_aline);
+}
 #endif
