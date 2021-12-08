c55dc7a5b91221a32b5288eaf4ffa8e4e277cecb
Meco Man <920369182@qq.com>
2021-12-08 12:20:24 -0500

    [rt-thread] 接管内存分配,实现命令行启动pika方式（用于后续支持终端键入）

--- package/pikaRTThread/pika_config.c
+++ package/pikaRTThread/pika_config.c
@@ -11,6 +11,10 @@
 #include <rtthread.h>
 #include "pikaPlatform.h"
 
+#ifndef RT_USING_HEAP
+#error "Please enable heap in the RT-Thread configuration"
+#endif
+
 int __platform_sprintf(char* buff, char* fmt, ...) {
     va_list args;
     int res;
@@ -24,4 +28,10 @@ int __platform_vsprintf(char* buff, char* fmt, va_list args){
 }
 int __platform_vsnprintf(char* buff, size_t size, const char* fmt, va_list args){
     return rt_vsnprintf(buff, size, fmt, args);
+}
+void* __platform_malloc(size_t size) {
+    return rt_malloc(size);
+}
+void __platform_free(void* ptr) {
+    rt_free(ptr);
 }
