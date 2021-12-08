1001f0d154661b2ae852e92fd01113e5fb441510
Meco Man <920369182@qq.com>
2021-12-08 08:03:29 -0500

    [rtthread] 重新实现rtthread软件包初始化部分

---
+++ package/rtthread/rt_pika.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2021-12-07     lyon       the first version
+ */
+#include <rtthread.h>
+#include <pikaScript.h>
+
+
+#define PIKASCRIPT_STACK_SIZE   1024
+#define PIKASCRIPT_STACK_PRIO   20
+
+static void pikascript_entry(void *parameter)
+{
+    pikaScriptInit();
+}
+
+static int rt_pika_init(void)
+{
+    rt_thread_t tid;
+    tid = rt_thread_create("PikaScript",
+                            pikascript_entry, RT_NULL,
+                            PIKASCRIPT_STACK_SIZE,
+                            PIKASCRIPT_STACK_PRIO, 0);
+
+    if (tid != RT_NULL)
+    {
+        rt_thread_startup(tid);
+    }
+    else
+    {
+
+    }
+
+    return 0;
+}
+
+INIT_APP_EXPORT(rt_pika_init);
