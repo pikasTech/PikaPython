8394bc03e17ef2c24f139e97ff2b86891927b66b
Man, Jianting (Meco) <920369182@qq.com>
2021-12-08 22:19:47 -0500

    增加软件包选项

--- package/pikaRTThread/rt_pika.c
+++ package/pikaRTThread/rt_pika.c
@@ -10,9 +10,19 @@
 #include <rtthread.h>
 #include <pikaScript.h>
 
-#if 0
+#ifdef PKG_PIKASCRIPT_USING_AUTORUNNING
+
+#ifdef PKG_PIKASCRIPT_STACK_SIZE
+#define PIKASCRIPT_STACK_SIZE   PKG_PIKASCRIPT_STACK_SIZE
+#else
 #define PIKASCRIPT_STACK_SIZE   4096
+#endif
+
+#ifdef PKG_PIKASCRIPT_STACK_PRIO
+#define PIKASCRIPT_STACK_PRIO   PKG_PIKASCRIPT_STACK_PRIO
+#else
 #define PIKASCRIPT_STACK_PRIO   20
+#endif
 
 static void pikascript_entry(void *parameter)
 {
@@ -38,14 +48,15 @@ static int rt_pika_init(void)
 
     return 0;
 }
-
 INIT_APP_EXPORT(rt_pika_init);
 
 #else
+
 static int pika(int argc, char *argv[])
 {
     pikaScriptInit();
     return 0;
 }
 MSH_CMD_EXPORT(pika, run pikascript);
-#endif
+
+#endif /* PKG_PIKASCRIPT_USING_AUTORUNNING */
