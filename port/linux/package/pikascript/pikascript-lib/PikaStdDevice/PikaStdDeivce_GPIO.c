4188460560c0aa08691d1b6736fc052af2a6838c
lyon <liang6516@outlook.com>
2021-12-10 22:26:13 +0800

    update PikaStdDevice to latest

--- port/linux/package/pikascript/pikascript-lib/PikaStdDevice/PikaStdDeivce_GPIO.c
+++ port/linux/package/pikascript/pikascript-lib/PikaStdDevice/PikaStdDeivce_GPIO.c
@@ -1,11 +1,13 @@
 #include "BaseObj.h"
 #include "PikaStdDevice_GPIO.h"
 
+
 void PikaStdDevice_GPIO_init(PikaObj* self) {
     obj_setInt(self, "isEnable", 0);
     obj_setStr(self, "pin", "PA0");
     obj_setStr(self, "mode", "out");
     obj_setInt(self, "isOn", 0);
+    obj_setStr(self, "pull", "none");
 }
 
 void PikaStdDevice_GPIO_disable(PikaObj* self) {
@@ -36,12 +38,31 @@ void PikaStdDevice_GPIO_high(PikaObj* self) {
     obj_run(self, "platformHigh()");
 }
 
-void PikaStdDevice_GPIO_setMode(PikaObj* self, char* mode) {
-    obj_setStr(self, "mode", mode);
-    obj_run(self, "platformSetMode(mode)");
+int PikaStdDevice_GPIO_read(PikaObj *self){
+    obj_run(self, "platformRead()");
+    return obj_getInt(self, "readBuff");
 }
 
-void PikaStdDevice_GPIO_setPin(PikaObj* self, char* pinName) {
+void PikaStdDevice_GPIO_setMode(PikaObj* self, char* mode) {
+    if(strEqu(mode, "out")||strEqu(mode, "in")){
+        obj_setStr(self, "mode", mode);
+        obj_run(self, "platformSetMode()");
+    }else{
+        obj_setErrorCode(self, 1);
+        obj_setSysOut(self, "[error] GPIO mode should be 'out' or 'in'.");
+    }
+}
+
+void PikaStdDevice_GPIO_setPull(PikaObj *self, char * pull){
+    if(strEqu(pull, "up")||strEqu(pull, "down")||strEqu(pull, "none")){
+        obj_setStr(self, "pull", pull);
+    }else{
+        obj_setErrorCode(self, 1);
+        obj_setSysOut(self, "[error] GPIO pull should be 'up', 'down' or 'none'.");
+    }
+}
+
+void PikaStdDevice_GPIO_setPin(PikaObj* self, char *pinName) {
     obj_setStr(self, "pin", pinName);
 }
 
@@ -64,7 +85,7 @@ void PikaStdDevice_GPIO_platformHigh(PikaObj* self) {
     obj_setSysOut(self, "[error] platform method need to be override.");
 }
 
-void PikaStdDevice_GPIO_platformSetMode(PikaObj* self, char* mode) {
+void PikaStdDevice_GPIO_platformSetMode(PikaObj* self) {
     obj_setErrorCode(self, 1);
     obj_setSysOut(self, "[error] platform method need to be override.");
 }
@@ -75,6 +96,11 @@ void PikaStdDevice_GPIO_platformOff(PikaObj* self) {
 }
 
 void PikaStdDevice_GPIO_platformOn(PikaObj* self) {
+    obj_setErrorCode(self, 1);
+    obj_setSysOut(self, "[error] platform method need to be override.");
+}
+
+void PikaStdDevice_GPIO_platformRead(PikaObj *self){
     obj_setErrorCode(self, 1);
     obj_setSysOut(self, "[error] platform method need to be override.");
 }
