b7d130821091a426330de66955a56f6cdb9f2a44
Meco Man <920369182@qq.com>
2021-12-09 20:06:24 -0500

    remove "need to override"

--- package/pikaRTDevice/pikaRTDevice.py
+++ package/pikaRTDevice/pikaRTDevice.py
@@ -2,26 +2,20 @@ import PikaStdDevice
 
 
 class GPIO(PikaStdDevice.GPIO):
-    # need be overrid
     def platformHigh():
         pass
 
-    # need override
     def platformLow():
         pass
 
-    # need override
     def platformEnable():
         pass
 
-    # need override
     def platformDisable():
         pass
 
-    # need override
     def platformSetMode():
         pass
 
-    # need override
     def platformRead():
         pass
