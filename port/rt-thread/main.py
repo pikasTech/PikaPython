365d955074b96b86c2d1492f8aeec3d4f5bc8819
李昂 <645275593@qq.com>
2021-12-10 12:36:23 +0000

    update  port/rt-thread/main.py.

--- port/rt-thread/main.py
+++ port/rt-thread/main.py
@@ -2,8 +2,12 @@ import PikaStdLib
 import pikaRTThread
 
 thread = pikaRTThread.Thread()
+mem = PikaStdLib.MemChecker()
 i = 0
 while i < 3:
     i = i + 1
     print('hello PikaScript!')
     thread.mdelay(500)
+
+print('mem use max:')
+mem.max()
