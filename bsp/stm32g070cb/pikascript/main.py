42325582d511fe866da51949df240e0d0aebfa94
pikastech <liang6516@outlook.com>
2021-12-11 17:57:12 +0800

    remove dependency of stm32 package for g070

--- bsp/stm32g070cb/pikascript/main.py
+++ bsp/stm32g070cb/pikascript/main.py
@@ -1,27 +1,5 @@
-import STM32
 import PikaStdLib
 import PikaMath
 
-time = STM32.Time()
-uart = STM32.UART()
-adc = STM32.ADC()
-pin = STM32.GPIO()
-pwm = STM32.PWM()
-uart = STM32.UART()
-mem = PikaStdLib.MemChecker()
-op = PikaMath.Operator()
-
-uart.init()
-uart.setId(1)
-uart.setBaudRate(115200)
-uart.enable()
-
-
-print('hello 2')
+print('hello pikascript')
 print('mem used max:')
-mem.max()
-
-while True:
-    time.sleep_ms(10)
-    print('flowing...')
-
