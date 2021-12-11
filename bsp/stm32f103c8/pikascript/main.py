6b21a0c3c61010529848cd730bb2dc9093a9b49b
pikastech <liang6516@outlook.com>
2021-12-11 18:41:18 +0800

    update packages for f103c8

--- bsp/stm32f103c8/pikascript/main.py
+++ bsp/stm32f103c8/pikascript/main.py
@@ -1,26 +1,4 @@
-import STM32
+import STM32F1
 import PikaStdLib
-import PikaMath
 
-time = STM32.Time()
-uart = STM32.UART()
-adc = STM32.ADC()
-pin = STM32.GPIO()
-pwm = STM32.PWM()
-uart = STM32.UART()
-mem = PikaStdLib.MemChecker()
-op = PikaMath.Operator()
-
-uart.init()
-uart.setId(1)
-uart.setBaudRate(115200)
-uart.enable()
-
-print('hello 2')
-print('mem used max:')
-mem.max()
-
-while True:
-    time.sleep_ms(10)
-    print('flowing...')
 
