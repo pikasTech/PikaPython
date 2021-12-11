35fd4f264bae31456bd415542af195454968ade6
pikastech <liang6516@outlook.com>
2021-12-11 20:59:23 +0800

    update booter for f103c8 and printf support

--- package/STM32F103C8Booter/main.c
+++ package/STM32F103C8Booter/main.c
@@ -24,23 +24,9 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "pikaScript.h"
-/* USER CODE END Includes */
+UART_HandleTypeDef huart1;
+static void MX_USART1_UART_Init(void);
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 PikaObj *pikaMain;
 /* USER CODE BEGIN PV */
 
@@ -48,19 +34,12 @@ PikaObj *pikaMain;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
+int fputc(int ch, FILE* f) {
+    HAL_UART_Transmit(&huart1, (uint8_t*)&ch, sizeof(ch), HAL_MAX_DELAY);
+    return ch;
+}
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -84,6 +63,7 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
+  MX_USART1_UART_Init();
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
   pikaMain = pikaScriptInit();
@@ -138,10 +118,33 @@ void SystemClock_Config(void)
   }
 }
 
-/* USER CODE BEGIN 4 */
+static void MX_USART1_UART_Init(void)
+{
 
-/* USER CODE END 4 */
+  /* USER CODE BEGIN USART1_Init 0 */
 
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
 /**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
