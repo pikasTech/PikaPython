00a5d6fe38eb9c06a5e5dfc729f8783785d3b789
pikastech <liang6516@outlook.com>
2021-12-11 22:13:14 +0800

    update main.py add led demo

--- bsp/stm32f103rc/pikascript/main.py
+++ bsp/stm32f103rc/pikascript/main.py
@@ -2,3 +2,14 @@ import PikaStdLib
 import STM32F1
 
 print('hello PikaScript!')
+led = STM32F1.GPIO()
+led.setPin('PA8')
+led.setMode('out')
+led.enable()
+time = STM32F1.Time()
+
+while True:
+    led.high()
+    time.sleep_ms(500)
+    led.low()
+    time.sleep_ms(500)
