74426e4bd3e97060f06b2cb451a38042b5634abc
lyon <liang6516@outlook.com>
2021-12-13 21:32:45 +0800

    add PikaStdData

---
+++ package/PikaStdData/PikaStdData.py
@@ -0,0 +1,17 @@
+from PikaObj import *
+
+class List(TinyObj):
+    def init():
+        pass
+
+    def append(arg: any):
+        pass
+
+    def get(i: int) -> any:
+        pass
+
+    def set(i: int, arg: any):
+        pass
+
+    def len() -> int:
+        pass
