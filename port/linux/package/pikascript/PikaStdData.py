078eab2bfbbd10ba4c83e9f8448003a488dd8220
lyon <liang6516@outlook.com>
2021-12-13 21:44:05 +0800

    add pikastddata.py

---
+++ port/linux/package/pikascript/PikaStdData.py
@@ -0,0 +1,21 @@
+from PikaObj import *
+
+class List(TinyObj):
+    def __init__():
+        pass
+
+    # add an arg after the end of list
+    def append(arg: any):
+        pass
+
+    # get an arg by the index
+    def get(i: int) -> any:
+        pass
+
+    # set an arg by the index 
+    def set(i: int, arg: any):
+        pass
+
+    # get the length of list
+    def len() -> int:
+        pass
