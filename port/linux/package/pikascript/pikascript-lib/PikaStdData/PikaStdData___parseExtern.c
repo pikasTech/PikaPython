0945b66b872ed185b8a7b4b67ae8b5403e029f15
lyon <liang6516@outlook.com>
2021-12-20 16:15:40 +0800

    no Werror, use inner buffs

--- port/linux/package/pikascript/pikascript-lib/PikaStdData/PikaStdData___parseExtern.c
+++ port/linux/package/pikascript/pikascript-lib/PikaStdData/PikaStdData___parseExtern.c
@@ -2,5 +2,10 @@
 #include "dataStrs.h"
 
 char* __pikaExtern_beforeParseLine(Args* outBuffs, char* line) {
+    Args * buffs = New_strBuff();
+    // char* tokens = Lexer_getTokens(buffs, line);
+    // char* print_tokens = Lexer_printTokens(buffs, tokens);
+output:
+    args_deinit(buffs);
     return line;
 }
