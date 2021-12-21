30c122eecd2f835d9c67e3054b8ee1c477d469b8
vsfos <vsfos@foxmail.com>
2021-12-21 23:49:28 +0800

    add PikaVSF package

---
+++ package/PikaVSF/PikaVSF.py
@@ -0,0 +1,24 @@
+import PikaStdDevice
+
+// kernel
+// TODO
+
+// hal
+class GPIO(PikaStdDevice.GPIO):
+    def platformHigh():
+        pass
+
+    def platformLow():
+        pass
+
+    def platformEnable():
+        pass
+
+    def platformDisable():
+        pass
+
+    def platformSetMode():
+        pass
+
+    def platformRead():
+        pass
