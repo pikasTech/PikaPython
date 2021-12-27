d9b4666032204c0ae77d3c25bd2ff0518cb630fb
李昂 <645275593@qq.com>
2021-12-27 17:33:14 +0000

    update  port/rt-thread/main.py.

--- port/rt-thread/main.py
+++ port/rt-thread/main.py
@@ -3,9 +3,8 @@ import pikaRTThread
 
 thread = pikaRTThread.Thread()
 mem = PikaStdLib.MemChecker()
-i = 0
-while i < 3:
-    i = i + 1
+
+for i in range(0, 3):
     print('hello PikaScript!')
     thread.mdelay(500)
 
