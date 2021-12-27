500cbfc128352302252120e92f3dd7ba083da427
pikastech <liang6516@outlook.com>
2021-12-27 17:25:33 +0800

    fix printf_init of g070

--- bsp/stm32g070cb/pikascript/main.py
+++ bsp/stm32g070cb/pikascript/main.py
@@ -1,5 +1,7 @@
 import PikaStdLib
 import STM32G0
 
+mem = PikaStdLib.MemChecker()
 print('hello pikascript')
 print('mem used max:')
+mem.max()
--- package/STM32G070CBBooter/main.c
+++ package/STM32G070CBBooter/main.c
@@ -63,27 +63,11 @@ void SystemClock_Config(void);
   */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-  HARDWARE_PRINTF_Init();
-  /* USER CODE BEGIN Init */
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
+
+  HARDWARE_PRINTF_Init();    
   pikaMain = pikaScriptInit();  
   /* USER CODE END 2 */
 
