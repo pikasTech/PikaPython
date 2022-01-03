9e5ddd3364f4818de671defc2929c8560df89e5b
lyon <liang6516@outlook.com>
2022-01-04 00:46:35 +0800

    for in range 10000

--- port/linux/benchmark/main.cpp
+++ port/linux/benchmark/main.cpp
@@ -8,20 +8,19 @@ extern "C" {
 #include "pikaScript.h"
 }
 
-static void BM_for_continue(benchmark::State& state) {
+static void BM_for_app(benchmark::State& state) {
     for (auto _ : state) {
         PikaObj* pikaMain = newRootObj((char*)"pikaMain", New_PikaMain);
         /* run */
         obj_run(pikaMain, (char*)
          "a = 0\n"
-         "for i in range(0, 1000):\n"
-         "    if i == 5:\n"
-         "        continue\n"
-         "    a = a + i\n"
+         "for i in range(0, 10000):\n"
+         "    a = a + 1\n"
+         "print(a)\n"
          "\n"
         );
     }
 }
-BENCHMARK(BM_for_continue);
+BENCHMARK(BM_for_app);
 
 BENCHMARK_MAIN();
