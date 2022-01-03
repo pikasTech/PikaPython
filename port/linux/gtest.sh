1c2723a84d558bdab893d0fab2dc57544bb48a8f
lyon <liang6516@outlook.com>
2022-01-04 00:53:58 +0800

    add json

--- port/linux/gtest.sh
+++ port/linux/gtest.sh
@@ -1,5 +1,5 @@
 rm build/test/pikascript_test
 sh make.sh
 build/test/pikascript_test 
-build/benchmark/pikascript_benchmark
+build/benchmark/pikascript_benchmark --benchmark_format=json | tee benchmark_result.json
 #build/test/pikascript_test --gtest_filter=args_*
