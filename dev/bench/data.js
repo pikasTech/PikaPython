7452ce2bc35bcba8e0b6bb62ded05fbb1b1cce4a
github-action-benchmark <github@users.noreply.github.com>
2022-01-03 18:16:24 +0000

    add Benchmark (googlecpp) benchmark result for 29fae00549fc5cde965ced6a5e93f2233d93fb45

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1641233784799,
+  "repoUrl": "https://github.com/pikasTech/pikascript",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "88232613+pikasTech@users.noreply.github.com",
+            "name": "Lyon",
+            "username": "pikasTech"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29fae00549fc5cde965ced6a5e93f2233d93fb45",
+          "message": "Update CI.yml",
+          "timestamp": "2022-01-04T02:13:42+08:00",
+          "tree_id": "c86b28bc7fb750aa9aec2f3bddd0ec11f7710374",
+          "url": "https://github.com/pikasTech/pikascript/commit/29fae00549fc5cde965ced6a5e93f2233d93fb45"
+        },
+        "date": 1641233783666,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_for_app",
+            "value": 908508186.9999954,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 906412200.0000001 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
