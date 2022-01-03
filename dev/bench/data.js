59da59681b166da799987a420cc2fe35776af6fe
Lyon <88232613+pikasTech@users.noreply.github.com>
2022-01-04 02:38:51 +0800

    remove data

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -6,66 +6,6 @@ window.BENCHMARK_DATA = {
       {
         "commit": {
           "author": {
-            "email": "88232613+pikasTech@users.noreply.github.com",
-            "name": "Lyon",
-            "username": "pikasTech"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "29fae00549fc5cde965ced6a5e93f2233d93fb45",
-          "message": "Update CI.yml",
-          "timestamp": "2022-01-04T02:13:42+08:00",
-          "tree_id": "c86b28bc7fb750aa9aec2f3bddd0ec11f7710374",
-          "url": "https://github.com/pikasTech/pikascript/commit/29fae00549fc5cde965ced6a5e93f2233d93fb45"
-        },
-        "date": 1641233783666,
-        "tool": "googlecpp",
-        "benches": [
-          {
-            "name": "BM_for_app",
-            "value": 908508186.9999954,
-            "unit": "ns/iter",
-            "extra": "iterations: 1\ncpu: 906412200.0000001 ns\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "committer": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "distinct": true,
-          "id": "caaaddedae9569fc00a1457b90d1e37203b24bc4",
-          "message": "Merge branch 'master' of https://github.com/pikastech/pikascript",
-          "timestamp": "2022-01-04T02:22:27+08:00",
-          "tree_id": "3e99a0c44d5afb64a5571659614ccbff4c52be10",
-          "url": "https://github.com/pikasTech/pikascript/commit/caaaddedae9569fc00a1457b90d1e37203b24bc4"
-        },
-        "date": 1641234317172,
-        "tool": "googlecpp",
-        "benches": [
-          {
-            "name": "BM_for_app",
-            "value": 906079338.999973,
-            "unit": "ns/iter",
-            "extra": "iterations: 1\ncpu: 905934900 ns\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
             "email": "liang6516@outlook.com",
             "name": "lyon",
             "username": "pikasTech"
