32a73deaa753e432da3f9cd6cc07013b165fbebc
lyon <liang6516@outlook.com>
2022-01-04 01:11:26 +0800

    test

--- port/linux/gtest.sh
+++ port/linux/gtest.sh
@@ -1,5 +1,5 @@
 rm build/test/pikascript_test
 sh make.sh
 build/test/pikascript_test 
-build/benchmark/pikascript_benchmark --benchmark_format=json | tee benchmark_result.json
+build/benchmark/pikascript_benchmark --benchmark_format=json
 #build/test/pikascript_test --gtest_filter=args_*
