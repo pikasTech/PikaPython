238267e751bea0f585299d74d453b3c03e086234
github-action-benchmark <github@users.noreply.github.com>
2022-01-04 06:45:34 +0000

    add Benchmark (googlecpp) benchmark result for b44baf3cc20eb5f5bba12a835e99e5dc047162d2

---
+++ dev/bench/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1641278734805,
+  "repoUrl": "https://github.com/pikasTech/pikascript",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "liang6516@outlook.com",
+            "name": "lyon",
+            "username": "pikasTech"
+          },
+          "committer": {
+            "email": "liang6516@outlook.com",
+            "name": "lyon",
+            "username": "pikasTech"
+          },
+          "distinct": true,
+          "id": "b44baf3cc20eb5f5bba12a835e99e5dc047162d2",
+          "message": "add c_primer_100",
+          "timestamp": "2022-01-04T13:03:01+08:00",
+          "tree_id": "feca97c201298f6e7e02234ade995f9ca18e3dc4",
+          "url": "https://github.com/pikasTech/pikascript/commit/b44baf3cc20eb5f5bba12a835e99e5dc047162d2"
+        },
+        "date": 1641278732758,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "for_loop_10000",
+            "value": 1736.695050999998,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 1735.3790000000001 ms\nthreads: 1"
+          },
+          {
+            "name": "while_loop_10000",
+            "value": 1070.571866999984,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 1070.1441000000002 ms\nthreads: 1"
+          },
+          {
+            "name": "prime_number_100",
+            "value": 337.8464744999974,
+            "unit": "ms/iter",
+            "extra": "iterations: 2\ncpu: 337.81780000000003 ms\nthreads: 1"
+          },
+          {
+            "name": "prime_number_100_c",
+            "value": 0.005209915741982728,
+            "unit": "ms/iter",
+            "extra": "iterations: 131216\ncpu: 0.005209578100231679 ms\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
