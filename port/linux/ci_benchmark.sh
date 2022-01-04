b3918c6f38b91a4777575b028d48499fbc4f9642
lyon <liang6516@outlook.com>
2022-01-04 20:48:59 +0800

    not set time_min

--- port/linux/ci_benchmark.sh
+++ port/linux/ci_benchmark.sh
@@ -1,2 +1,2 @@
-Release/benchmark/pikascript_benchmark --benchmark_format=json --benchmark_min_time=5 | tee benchmark_result.json 
+Release/benchmark/pikascript_benchmark --benchmark_format=json | tee benchmark_result.json 
 python3 ci_benchmark_calibration.py
