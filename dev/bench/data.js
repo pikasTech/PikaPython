e5b7e561b5e2f41c6a6416b11dd2a5b9e76861fd
Lyon <88232613+pikasTech@users.noreply.github.com>
2022-01-04 09:30:55 +0800

    Update data.js

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,66 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "pikasTech"
           },
           "distinct": true,
-          "id": "a0b01fc346d2183ba0516ac510f1d56583b1ef2d",
-          "message": "test benchamrk",
-          "timestamp": "2022-01-04T02:42:49+08:00",
-          "tree_id": "484e82fbe2ffaa1ebbea3e5f13722b95d51051ec",
-          "url": "https://github.com/pikasTech/pikascript/commit/a0b01fc346d2183ba0516ac510f1d56583b1ef2d"
-        },
-        "date": 1641235579932,
-        "tool": "googlecpp",
-        "benches": [
-          {
-            "name": "BM_for_app",
-            "value": 1656518266.999967,
-            "unit": "ns/iter",
-            "extra": "iterations: 1\ncpu: 1653722500 ns\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "committer": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "distinct": true,
-          "id": "d5e44ce277a211448dacd4e65111d623dcc68ec8",
-          "message": "add install_goglebenchmark",
-          "timestamp": "2022-01-04T09:06:13+08:00",
-          "tree_id": "1dca4d1229caed7a6b4f8e482394712d41360b0e",
-          "url": "https://github.com/pikasTech/pikascript/commit/d5e44ce277a211448dacd4e65111d623dcc68ec8"
-        },
-        "date": 1641258601665,
-        "tool": "googlecpp",
-        "benches": [
-          {
-            "name": "BM_for_app",
-            "value": 1474450971.9999995,
-            "unit": "ns/iter",
-            "extra": "iterations: 1\ncpu: 1474345200 ns\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "committer": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "distinct": true,
           "id": "ed8bccc96cdb972665076aade82db83f8326f280",
           "message": "update ci_benchmark.sh",
           "timestamp": "2022-01-04T09:18:22+08:00",
