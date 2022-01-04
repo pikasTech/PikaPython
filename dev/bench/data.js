d95e138833622bc9543b4d5b0714d325a5df96d4
Lyon <88232613+pikasTech@users.noreply.github.com>
2022-01-04 09:29:19 +0800

    Update data.js

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -16,36 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "pikasTech"
           },
           "distinct": true,
-          "id": "60dc41554fec9b494a0516aa66f78d7d96011454",
-          "message": "test for loop",
-          "timestamp": "2022-01-04T02:24:59+08:00",
-          "tree_id": "b06638a2fe5e4b8c546eb6a7d74a8fece3ed884e",
-          "url": "https://github.com/pikasTech/pikascript/commit/60dc41554fec9b494a0516aa66f78d7d96011454"
-        },
-        "date": 1641234462928,
-        "tool": "googlecpp",
-        "benches": [
-          {
-            "name": "BM_for_app",
-            "value": 1488792925.0000126,
-            "unit": "ns/iter",
-            "extra": "iterations: 1\ncpu: 1484679100.0000002 ns\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "committer": {
-            "email": "liang6516@outlook.com",
-            "name": "lyon",
-            "username": "pikasTech"
-          },
-          "distinct": true,
           "id": "a0b01fc346d2183ba0516ac510f1d56583b1ef2d",
           "message": "test benchamrk",
           "timestamp": "2022-01-04T02:42:49+08:00",
