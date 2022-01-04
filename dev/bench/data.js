c1e796072bee4d654507fbb49d92975332949e1b
github-action-benchmark <github@users.noreply.github.com>
2022-01-04 04:12:46 +0000

    add Benchmark (googlecpp) benchmark result for e7a982c4f4981f1263d0576e4c0c84d10f2adc9b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1641267685825,
+  "lastUpdate": 1641269566154,
   "repoUrl": "https://github.com/pikasTech/pikascript",
   "entries": {
     "Benchmark": [
@@ -210,6 +210,48 @@ window.BENCHMARK_DATA = {
             "value": 285286179.99999994,
             "unit": "ns/iter",
             "extra": "iterations: 2\ncpu: 285271649.9999999 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "liang6516@outlook.com",
+            "name": "lyon",
+            "username": "pikasTech"
+          },
+          "committer": {
+            "email": "liang6516@outlook.com",
+            "name": "lyon",
+            "username": "pikasTech"
+          },
+          "distinct": true,
+          "id": "e7a982c4f4981f1263d0576e4c0c84d10f2adc9b",
+          "message": "use ms instead of us",
+          "timestamp": "2022-01-04T12:09:59+08:00",
+          "tree_id": "51c96e1a8d35fee66b530bd2b9d04a2bb7fae0eb",
+          "url": "https://github.com/pikasTech/pikascript/commit/e7a982c4f4981f1263d0576e4c0c84d10f2adc9b"
+        },
+        "date": 1641269563718,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "for_loop_10000",
+            "value": 1477.6735079999526,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 1476.8539999999998 ms\nthreads: 1"
+          },
+          {
+            "name": "while_loop_10000",
+            "value": 907.3914580000064,
+            "unit": "ms/iter",
+            "extra": "iterations: 1\ncpu: 907.1759999999999 ms\nthreads: 1"
+          },
+          {
+            "name": "prime_number_100",
+            "value": 287.7032394999901,
+            "unit": "ms/iter",
+            "extra": "iterations: 2\ncpu: 287.68045000000006 ms\nthreads: 1"
           }
         ]
       }
