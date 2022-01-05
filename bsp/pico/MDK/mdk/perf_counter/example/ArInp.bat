f5b1448d51bb7346c3f5267a098c223463194e3b
pikastech <liang6516@outlook.com>
2022-01-05 10:26:06 +0800

    delete perf_counter

--- bsp/pico/MDK/mdk/perf_counter/example/ArInp.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\GNU Arm Embedded Toolchain\10 2020-q4-major\bin\arm-none-eabi-ar" -M <ArInp.Scr
--- bsp/pico/MDK/mdk/perf_counter/example/copy.bat
+++
@@ -1,3 +0,0 @@
-@copy /B /Y .\Out\*.lib ..\lib\
-@copy /B /Y .\GCC_Out\*.a ..\lib\
-@copy /B /Y ..\perf_counter.h ..\lib\
