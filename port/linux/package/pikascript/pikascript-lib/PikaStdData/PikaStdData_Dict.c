79fed360b992d7acb1cc1d76f138908d12e3fe92
lyon <liang6516@outlook.com>
2022-01-06 10:56:14 +0800

    use pyload instead of self in dict

--- port/linux/package/pikascript/pikascript-lib/PikaStdData/PikaStdData_Dict.c
+++ port/linux/package/pikascript/pikascript-lib/PikaStdData/PikaStdData_Dict.c
@@ -1,23 +1,30 @@
 #include "PikaObj.h"
 #include "PikaStdLib_SysObj.h"
 #include "PikaStdData_Dict.h"
+#include "BaseObj.h"
 
 Arg* PikaStdData_Dict_get(PikaObj* self, char* key) {
-    return arg_copy(obj_getArg(self, key));
+    PikaObj* pyload = obj_getObj(self, "pyload", 0);
+    return arg_copy(obj_getArg(pyload, key));
 }
 
-void PikaStdData_Dict___init__(PikaObj* self) {}
+void PikaStdData_Dict___init__(PikaObj* self) {
+    obj_newObj(self, "pyload", "", New_TinyObj);
+}
 
 void PikaStdData_Dict_set(PikaObj* self, Arg* arg, char* key) {
-    obj_setArg(self, key, arg);
+    PikaObj* pyload = obj_getObj(self, "pyload", 0);
+    obj_setArg(pyload, key, arg);
 }
 
 void PikaStdData_Dict_remove(PikaObj* self, char* key) {
-    PikaStdLib_SysObj_remove(self, key);
+    PikaObj* pyload = obj_getObj(self, "pyload", 0);
+    PikaStdLib_SysObj_remove(pyload, key);
 }
 
 Arg * PikaStdData_Dict___iter__(PikaObj *self){
-    return NULL;
+    obj_setInt(self, "__iter_i", 0);
+    return arg_setPtr(NULL, "", TYPE_POINTER, self);
 }
 Arg * PikaStdData_Dict___next__(PikaObj *self){
     return NULL;
