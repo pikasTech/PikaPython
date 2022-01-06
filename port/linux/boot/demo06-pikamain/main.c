406fdbf92963b38d7816043d6a71408e28b46e9c
lyon <liang6516@outlook.com>
2022-01-06 16:17:40 +0800

    add demo_pikaMain to test shell

--- port/linux/boot/demo06-pikamain/main.c
+++ port/linux/boot/demo06-pikamain/main.c
@@ -1,39 +1,7 @@
 /* this demo shows the usage of method */
-#include "pikaScript.h"
 #include <stdio.h>
+#include "pikaScript.h"
 
-void obj_runWithInfo(PikaObj *self, char *cmd)
-{
-    printf(">>> %s\r\n", cmd);
-    obj_run(self, cmd);
-}
-
-int main()
-{
-    PikaObj *pikaMain = pikaScriptInit();
-    /* user input buff */
-    char inputBuff[256] = {0};
-    /* run the script with check*/
-    printf(">>> ");
-    while (1)
-    {
-        /* get user input */
-        fgets(inputBuff, sizeof(inputBuff), stdin);
-
-        /* run PikaScript and get res */
-        PikaObj *globals = obj_runDirect(pikaMain, inputBuff);
-
-        /* get system output of PikaScript*/
-        char *sysOut = args_getSysOut(globals->list);
-
-        if (!strEqu("", sysOut))
-        {
-            /* print out the system output */
-            printf("%s\r\n", sysOut);
-        }
-        printf(">>> ");
-
-        /* deinit the res */
-        // obj_deinit(globals);
-    }
+int main() {
+    pikaScriptShell(pikaScriptInit());
 }
