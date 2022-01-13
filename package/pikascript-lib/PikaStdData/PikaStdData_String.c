4dc39ecfcdbbed2dd990006cc57ceac2587fcfa8
lyon <liang6516@outlook.com>
2022-01-13 16:38:12 +0800

    remove garbage

--- package/pikascript-lib/PikaStdData/PikaStdData_String.c
+++
@@ -1,9 +0,0 @@
-#include "PikaObj.h"
-
-Arg* PikaStdData_String___iter__(PikaObj* self){
-    return arg_setNull(NULL);
-}
-
-Arg* PikaStdData_String___next__(PikaObj* self){
-    return arg_setNull(NULL);
-}
