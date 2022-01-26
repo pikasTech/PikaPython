0d3a56377d7af6dd822f0f67947a94359d01c249
lyon <liang6516@outlook.com>
2022-01-26 16:18:31 +0800

    use the same main.py in document for keil simulation

--- bsp/simulation-keil/pikascript/main.py
+++ bsp/simulation-keil/pikascript/main.py
@@ -2,12 +2,14 @@ from PikaObj import *
 import Device
 import PikaStdLib
 
+led = Device.LED()
+uart = Device.Uart()
 mem = PikaStdLib.MemChecker()
 
-
-for i in range(1, 10):
-    print(i)
-
+print('hello wrold')
+uart.setName('com1')
+uart.send('My name is:')
+uart.printName()
 print('mem used max:')
 mem.max()
 print('mem used now:')
