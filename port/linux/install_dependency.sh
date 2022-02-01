266d70098b28f8907469ee7c1719354b0f8322de
ubuntu <ubuntu@localhost.localdomain>
2022-02-01 18:25:51 +0800

    improve install_depencency

--- port/linux/install_dependency.sh
+++ port/linux/install_dependency.sh
@@ -1,3 +1,5 @@
+apt install cmake -y
+apt install g++ -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 git clone https://github.com/google/benchmark.git google_benchmark && cd google_benchmark && cmake -E make_directory "build" && cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../ && cmake --build "build" --config Release && sudo cmake --build "build" --config Release --target install
