fba650a737a38be465fad83232739d44bc5a2ad4
lyon1998 <liang6516@outlook.com>
2022-02-11 01:34:00 +0800

    use filter.py to update data.js

--- dev/bench/filter.py
+++ dev/bench/filter.py
@@ -3,25 +3,30 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 
-def get_benchmark_list()->list:
-    data_file = open('data.js','r')
-    data_js_prefix = "window.BENCHMARK_DATA = "
+data_js_prefix = "window.BENCHMARK_DATA = "
+
+def get_data_json(path:str):
+    data_file = open(path,'r')
     data_json_str = data_file.read().replace(data_js_prefix, "")
-    data_json = json.loads(data_json_str)
+    return json.loads(data_json_str)
+
+
+def get_benchmark_list(path:str)->list:
+    data_json = get_data_json(path)
     return data_json['entries']['Benchmark']
 
 
-def get_perferance_point_list()->list:
-    benchmark_list = get_benchmark_list()
+def get_perferance_point_list(path:str)->list:
+    benchmark_list = get_benchmark_list(path)
     perferance_point_list:list = []
-    for i in range(5, benchmark_list.__len__() - 5):
+    for i in range(benchmark_list.__len__()):
         benchmark = benchmark_list[i]
         benches:list = benchmark['benches']
         # for j in range(0, benches.__len__()):
         perferance_point_list.append(benches[0]['value'])
     return perferance_point_list
 
-p_list = get_perferance_point_list()
+p_list = get_perferance_point_list('data.js')
 
 median_range = 10
 
@@ -44,8 +49,22 @@ for i in range(median_range//2, p_list.__len__() - median_range//2):
 for i in range(p_list.__len__() - median_range//2, p_list.__len__()):
     p_list_after.append(p_list[i])
 
-plt.plot(p_list)
-plt.plot(p_list_after)
+def set_data_json_after(p_list_after:list):
+    data_json = get_data_json('data.js')
+    benchmark_list = get_benchmark_list('data.js')
+    for i in range(benchmark_list.__len__()):
+        data_json['entries']['Benchmark'][i]['benches'][0]['value'] = p_list_after[i]
+    data_json_after_str = json.dumps(data_json)
+    data_json_after_str = data_js_prefix + data_json_after_str
+    file_out = open('data_after.js', 'w')
+    file_out.write(data_json_after_str)
+
+set_data_json_after(p_list_after)
+
+# data_json_after = set_data_json_after(p_list_after)
+
+plt.plot(get_perferance_point_list('data.js'))
+plt.plot(get_perferance_point_list('data_after.js'))
 plt.show()
 # for each benchmark
 
