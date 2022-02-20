a59c85b3d7272ebac8e2921d705d88a49ba450f5
lyon <liang6516@outlook.com>
2022-02-20 20:19:56 +0800

    add machine to combien g0 and pikapizero pkg

---
+++ bsp/stm32g030c8/pikascript/machine.py
@@ -0,0 +1,42 @@
+import STM32G0
+import PikaPiZero
+
+
+class GPIO(STM32G0.GPIO):
+    pass
+
+
+class Time(STM32G0.Time):
+    pass
+
+
+class ADC(STM32G0.ADC):
+    pass
+
+
+class UART(STM32G0.UART):
+    pass
+
+
+class PWM(STM32G0.PWM):
+    pass
+
+
+class Task(STM32G0.Task):
+    pass
+
+
+class RGB(PikaPiZero.RGB):
+    pass
+
+
+class KEY(PikaPiZero.KEY):
+    pass
+
+
+class LCD(PikaPiZero.LCD):
+    pass
+
+
+class Point(PikaPiZero.Point):
+    pass
--- bsp/stm32g030c8/pikascript/main.py
+++ bsp/stm32g030c8/pikascript/main.py
@@ -1,13 +1,12 @@
-import STM32G0
-import PikaPiZero
 import PikaStdLib
+import machine
 
-time = STM32G0.Time()
-adc = STM32G0.ADC()
-pin = STM32G0.GPIO()
-pwm = STM32G0.PWM()
-uart = STM32G0.UART()
-rgb = PikaPiZero.RGB()
+time = machine.Time()
+adc = machine.ADC()
+pin = machine.GPIO()
+pwm = machine.PWM()
+uart = machine.UART()
+rgb = machine.RGB()
 mem = PikaStdLib.MemChecker()
 
 rgb.init()
