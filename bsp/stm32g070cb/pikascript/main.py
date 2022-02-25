fb98f85e57f03e914c787d32531d925ec5f6c5a3
李昂 <645275593@qq.com>
2022-02-25 15:53:26 +0000

    update  bsp/stm32g070cb/pikascript/main.py.

--- bsp/stm32g070cb/pikascript/main.py
+++ bsp/stm32g070cb/pikascript/main.py
@@ -1,6 +1,10 @@
 import PikaStdLib
 import Arm2D
 import machine
+import PikaStdTask
+import PikaStdData
+import PikaDebug
+
 print('hello pikascript!')
 mem = PikaStdLib.MemChecker()
 print('mem used max:')
