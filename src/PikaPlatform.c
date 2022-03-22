c5a088d97f14a0fbc265355c32894682d90d4cfd
lyon1998 <liang6516@outlook.com>
2022-03-22 14:37:12 +0800

    format

--- src/PikaPlatform.c
+++ src/PikaPlatform.c
@@ -38,7 +38,7 @@ PIKA_WEAK void __platform_enable_irq_handle(void) {
 PIKA_WEAK void* __platform_malloc(size_t size) {
     return malloc(size);
 }
-PIKA_WEAK void __platform_error_handle(void){
+PIKA_WEAK void __platform_error_handle(){
     return;
 }
 PIKA_WEAK void __platform_free(void* ptr) {
