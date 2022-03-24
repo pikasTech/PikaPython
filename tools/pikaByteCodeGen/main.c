4ddb60726a5333d3aa6d7f586f0e0841c539fffb
lyon <liang6516@outlook.com>
2022-03-24 18:04:32 +0800

    update bytecode_gen to v1.5.2

--- tools/pikaByteCodeGen/main.c
+++ tools/pikaByteCodeGen/main.c
@@ -5,6 +5,8 @@
 #include "PikaParser.h"
 #include "dataStrs.h"
 
+PikaObj* __pikaMain;
+
 void main() {
     FILE* file_mian_py = fopen("main.py", "rb");
     if (NULL == file_mian_py) {
@@ -29,55 +31,3 @@ void main() {
 
     return;
 }
-
-
-/* portable */
-void __platformDisableIrqHandle(void) {
-    /* disable irq to support thread */
-}
-void __platformEnableIrqHandle(void) {
-    /* disable irq to support thread */
-}
-void* __platformMalloc(size_t size) {
-    return malloc(size);
-}
-void __platformFree(void* ptr) {
-    free(ptr);
-}
-void* __impl_pikaMalloc(size_t size) {
-    return __platformMalloc(size);
-}
-void __impl_pikaFree(void* ptrm, size_t size) {
-    __platformFree(ptrm);
-}
-void __platformPoolInit(void) {}
-uint8_t __is_quick_malloc(void) {
-    return 0;
-}
-void __quick_malloc_enable(void) {}
-void __quick_malloc_disable(void) {}
-uint8_t __isLocked_pikaMemory(void) {
-    return 0;
-}
-void __platformPrintf(char* fmt, ...) {
-    va_list args;
-    va_start(args, fmt);
-    vprintf(fmt, args);
-    va_end(args);
-}
-char* __platformLoadPikaAsm(void) {
-    /* faild */
-    return NULL;
-}
-int32_t __platformSavePikaAsm(char* PikaAsm) {
-    /* faild */
-    return 1;
-}
-uint8_t __platformAsmIsToFlash(char* pyMultiLine) {
-    /* not to flash */
-    return 0;
-}
-int32_t __platformSavePikaAsmEOF(void) {
-    return 1;
-}
-
--- tools/pikaByteCodeGen/main.py
+++ tools/pikaByteCodeGen/main.py
@@ -1,28 +1,95 @@
-time = STM32.Time()
-uart = STM32.UART()
-adc = STM32.ADC()
-pin = STM32.GPIO()
-pwm = STM32.PWM()
-uart = STM32.UART()
-rgb = PikaPiZero.RGB()
+import PikaStdLib
+import PikaStdData
+
 mem = PikaStdLib.MemChecker()
-op = PikaMath.Operator()
+mem.resetMax()
+def EXPECT_EQ(test_name, input, expected):
+    print('-----TEST-----')
+    print(test_name)
+    if input != expected:
+        print('[Error]')
+        print(test_name)
+        print('[info] Expected:')
+        print(expected)
+        print('[info] Input:')
+        print(input)
+        # error
+        while True:
+            is_error = 1
+    else:
+        print('[ OK  ]')
 
-uart.init()
-uart.setId(1)
-uart.setBaudRate(115200)
-uart.enable()
+a = 0
+for i in range(0, 10):
+    if i == 5:
+        continue
+    a = a + i
+EXPECT_EQ('for_if_continue', a, 40)
+         
+a = 0
+for i in range(0, 10):
+    if i == 5:
+        break
+    a = a + i
+EXPECT_EQ('for_if_break', a, 10)
 
-rgb.init()
-rgb.enable()
+a = 0
+for i in range(0, 10):
+    for k in range(0, 3):
+        # print(k)
+        a = a + k
+EXPECT_EQ('for_for_in_range', a, 30)
 
-print('hello 2')
+a = 0
+for i in range(0, 10):
+    # print(i)
+    a = a + i
+EXPECT_EQ('for_in_range', a, 45)
+
+list = PikaStdData.List()
+list.append(7)
+list.append('eee')
+len = list.len()
+for item in list:
+    # print(item)
+    a = item
+EXPECT_EQ('list_for_loop', a, 'eee')
+
+
+list = PikaStdData.List()
+list.append(7)
+list.append('eee')
+len = list.len()
+a1 = list.get(0)
+a2 = list.get(1)
+res = iter(list)
+EXPECT_EQ('iter', res, list)
+
+dict = PikaStdData.Dict()
+dict.set('a', 1)
+dict.set('b', 2)
+dict.remove('b')
+a = dict.get('a')
+b = dict.get('b')
+EXPECT_EQ('PikaStdData', a, 1)
+EXPECT_EQ('PikaStdData_2', b, 0)
+
+num = 0
+i = 2
+for i in range(2,30):
+    j=2
+    is_prime = 1
+    for j in range(2,i):
+        if i%j==0 :
+            is_prime = 0
+            break
+    if is_prime:
+        num = num + i
+EXPECT_EQ('prime_number_30', num, 129)
+
+
+print('====================')
+print('[Unit Tests Succeed]')
+print('====================')
 print('mem used max:')
 mem.max()
-
-while True:
-    time.sleep_ms(10)
-    rgb.flow()
-    print('flowing')
-
-
