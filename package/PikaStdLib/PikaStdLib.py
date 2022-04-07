8fb6fbf0c6b93329e452f00512f76083a33f6d0a
lyon1998 <liang6516@outlook.com>
2022-04-07 23:21:07 +0800

    update PikaStdLib.py

--- package/PikaStdLib/PikaStdLib.py
+++ package/PikaStdLib/PikaStdLib.py
@@ -2,58 +2,58 @@ from PikaObj import *
 
 
 class MemChecker(TinyObj):
-    def max():
+    def max(self):
         pass
 
-    def now():
+    def now(self):
         pass
 
-    def getMax() -> float:
+    def getMax(self) -> float:
         pass
 
-    def getNow() -> float:
+    def getNow(self) -> float:
         pass
 
-    def resetMax():
+    def resetMax(self):
         pass
 
 
 class SysObj(BaseObj):
-    def type(arg: any):
+    def type(self, arg: any):
         pass
 
-    def remove(argPath: str):
+    def remove(self, argPath: str):
         pass
 
-    def int(arg: any) -> int:
+    def int(self, arg: any) -> int:
         pass
 
-    def float(arg: any) -> float:
+    def float(self, arg: any) -> float:
         pass
 
-    def str(arg: any) -> str:
+    def str(self, arg: any) -> str:
         pass
 
-    def iter(arg: any) -> any:
+    def iter(self, arg: any) -> any:
         pass
 
-    def range(a1: int, a2: int) -> any:
+    def range(self, a1: int, a2: int) -> any:
         pass
 
-    def __set__(obj: any, key: any, val: any, obj_str: str):
+    def __set__(self, obj: any, key: any, val: any, obj_str: str):
         pass
 
-    def __get__(obj: any, key: any) -> any:
+    def __get__(self, obj: any, key: any) -> any:
         pass
 
 
 class RangeObj(TinyObj):
-    def __next__() -> any:
+    def __next__(self) -> any:
         pass
 
 
 class StringObj(TinyObj):
-    def __next__() -> any:
+    def __next__(self) -> any:
         pass
 
 class PikaObj(TinyObj):
