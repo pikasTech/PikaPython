d18bc16aae183cb4e3d3b681d43b087be2563035
lyon1998 <liang6516@outlook.com>
2022-04-07 22:59:57 +0800

    use 'self' in stddata

--- port/linux/package/pikascript/PikaStdData.py
+++ port/linux/package/pikascript/PikaStdData.py
@@ -2,96 +2,96 @@ from PikaObj import *
 
 
 class List(TinyObj):
-    def __init__():
+    def __init__(self):
         pass
 
     # add an arg after the end of list
-    def append(arg: any):
+    def append(self, arg: any):
         pass
 
     # get an arg by the index
-    def get(i: int) -> any:
+    def get(self, i: int) -> any:
         pass
 
     # set an arg by the index
-    def set(i: int, arg: any):
+    def set(self, i: int, arg: any):
         pass
 
     # get the length of list
-    def len() -> int:
+    def len(self) -> int:
         pass
     # support for loop
-    def __iter__() -> any:
+    def __iter__(self) -> any:
         pass
 
     # support for loop
-    def __next__() -> any:
+    def __next__(self) -> any:
         pass
 
     # support list[]  = val
-    def __set__(__key: any, __val: any):
+    def __set__(self, __key: any, __val: any):
         pass
 
     # support val = list[] 
-    def __get__(__key: any) -> any:
+    def __get__(self, __key: any) -> any:
         pass
 
 class Dict(TinyObj):
-    def __init__():
+    def __init__(self):
         pass
 
     # get an arg by the key
-    def get(key: str) -> any:
+    def get(self, key: str) -> any:
         pass
 
     # set an arg by the key
-    def set(key: str, arg: any):
+    def set(self, key: str, arg: any):
         pass
 
     # remove an arg by the key
-    def remove(key: str):
+    def remove(self, key: str):
         pass
 
-    def __iter__() -> any:
+    def __iter__(self) -> any:
         pass
 
-    def __next__() -> any:
+    def __next__(self) -> any:
         pass
 
     # support dict[]  = val
-    def __set__(__key: any, __val: any):
+    def __set__(self, __key: any, __val: any):
         pass
 
     # support val = dict[] 
-    def __get__(__key: any) -> any:
+    def __get__(self, __key: any) -> any:
         pass
 
 
 class String(TinyObj):
-    def __init__(s:str):
+    def __init__(self, s:str):
         pass
 
-    def set(s:str):
+    def set(self, s:str):
         pass
 
-    def get()->str:
+    def get(self)->str:
         pass
 
-    def __iter__() -> any:
+    def __iter__(self) -> any:
         pass
 
-    def __next__() -> any:
+    def __next__(self) -> any:
         pass
 
     # support string[]  = val
-    def __set__(__key: any, __val: any):
+    def __set__(self, __key: any, __val: any):
         pass
 
     # support val = string[] 
-    def __get__(__key: any) -> any:
+    def __get__(self, __key: any) -> any:
         pass
 
 class ByteArray(List):
     # convert a string to ByteArray
-    def fromString(s:str):
+    def fromString(self, s:str):
         pass
