562550dce02351b58f072f248fc79f23e7b331b7
lyon1998 <liang6516@outlook.com>
2022-04-08 23:52:46 +0800

    cancel api-make-win10

--- port/linux/api-make.sh
+++ port/linux/api-make.sh
@@ -1,2 +1,2 @@
 sh api-make-linux.sh
-sh api-make-win10.sh
+# sh api-make-win10.sh
--- port/linux/update-compiler.sh
+++ port/linux/update-compiler.sh
@@ -4,4 +4,4 @@ cargo build
 cd $ROOT
 cp ../../tools/pikaCompiler/rust-msc-latest-linux package/pikascript/rust-msc-latest-linux
 cp ../../tools/pikaCompiler/target/debug/rust-msc package/pikascript/rust-msc-latest-linux
-cp ../../tools/pikaCompiler/target/debug/rust-msc.exe package/pikascript/rust-msc-latest-win10.exe
+# cp ../../tools/pikaCompiler/target/debug/rust-msc.exe package/pikascript/rust-msc-latest-win10.exe
