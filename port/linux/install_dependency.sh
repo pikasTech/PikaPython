168a527ebdd7e34b0b02d877dd3e2b353ea855b6
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2022-04-08 12:53:26 +0200

    Execute apt commands as sudo

--- port/linux/install_dependency.sh
+++ port/linux/install_dependency.sh
@@ -1,9 +1,9 @@
-apt install cmake -y
-apt install g++ -y
+sudo apt install cmake -y
+sudo apt install g++ -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 git clone https://github.com/google/benchmark.git google_benchmark && cd google_benchmark && cmake -E make_directory "build" && cmake -E chdir "build" cmake -DBENCHMARK_DOWNLOAD_DEPENDENCIES=on -DCMAKE_BUILD_TYPE=Release ../ && cmake --build "build" --config Release && sudo cmake --build "build" --config Release --target install
-apt install ninja-build -y
+sudo apt install ninja-build -y
 
 rm google_benchmark -rf
 
