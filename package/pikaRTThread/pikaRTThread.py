90bf6a0e49944a5c003dc849a9b108587f3ad2dd
李昂 <645275593@qq.com>
2022-04-11 10:39:35 +0000

    update  package/pikaRTThread/pikaRTThread.py.

--- package/pikaRTThread/pikaRTThread.py
+++ package/pikaRTThread/pikaRTThread.py
@@ -1,6 +1,11 @@
 from PikaObj import *
-
+import PikaStdTask
 
 class Thread(TinyObj):
     def mdelay(ms: int):
+        pass
+
+class Task(PikaStdTask.Task):
+    # overried
+    def platformGetTick(self):
         pass
