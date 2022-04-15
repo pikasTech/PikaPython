dabf78f39d6e49f05535b4aef64bffe397f657f5
lyon1998 <liang6516@outlook.com>
2022-04-15 21:09:18 +0800

    fix build scripts, add fast_test

---
+++ port/linux/fast_test.sh
@@ -0,0 +1,1 @@
+cd build && rm ./test/pikascript_test &&  ninja -j0 && ./test/pikascript_test
--- port/linux/make.sh
+++ port/linux/make.sh
@@ -1,4 +1,4 @@
 sh api-make.sh
-cp config/pika_config_benchmark.h config/pika_config.h 
 cd build
+ninja clean
 ninja -j0
