c193c37af3ae1bc729417526b1b99f3cef9b5e40
lyon1998 <liang6516@outlook.com>
2022-04-15 21:10:30 +0800

    fix gtest.sh

--- port/linux/gtest.sh
+++ port/linux/gtest.sh
@@ -1,4 +1,4 @@
 cp config/pika_config_default.h config/pika_config.h && sh _gtest_once.sh && \
 cp config/pika_config_builtin_list.h config/pika_config.h && sh _gtest_once.sh && \
-cp config/pika_config_benchmark.h config/pika_config.h && sh _gtest_once.sh 
-rm pika_config.h
+cp config/pika_config_benchmark.h config/pika_config.h && sh _gtest_once.sh && \
+rm config/pika_config.h
