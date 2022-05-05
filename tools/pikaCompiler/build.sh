bd1a491b3e481c97def3cf9a6bee677cbc9e577a
lyon <liang6516@outlook.com>
2022-05-05 16:18:16 +0800

    improve build.sh

--- tools/pikaCompiler/build.sh
+++ tools/pikaCompiler/build.sh
@@ -3,3 +3,5 @@ mkdir libout
 cargo build --release
 cbindgen --config cbindgen.toml --crate rust-msc --output libout/libpikabinder.h
 cp target/release/libpikabinder.a libout
+cp target/release/rust-msc rust-msc-latest-linux
+chmod +x rust-msc-latest-linux
