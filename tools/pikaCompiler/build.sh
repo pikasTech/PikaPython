04725f09967b5325d9447ea1247e8dd1973a930b
lyon <liang6516@outlook.com>
2022-05-05 16:16:08 +0800

    build libpikabind is ok

--- tools/pikaCompiler/build.sh
+++ tools/pikaCompiler/build.sh
@@ -1,2 +1,5 @@
+rm libout -rf
+mkdir libout
 cargo build --release
-cbindgen --config cbindgen.toml --crate rust-msc --output pika_binder.h
+cbindgen --config cbindgen.toml --crate rust-msc --output libout/libpikabinder.h
+cp target/release/libpikabinder.a libout
