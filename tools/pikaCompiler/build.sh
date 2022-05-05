541b775fd4ae9a5fe139c06d9884e255ba570d18
lyon <liang6516@outlook.com>
2022-05-05 16:36:36 +0800

    link pikabinder is ok

--- tools/pikaCompiler/build.sh
+++ tools/pikaCompiler/build.sh
@@ -1,7 +1,17 @@
-rm libout -rf
-mkdir libout
+ROOT=$PWD
+rm libpikabinder -rf
+mkdir libpikabinder
 cargo build --release
-cbindgen --config cbindgen.toml --crate rust-msc --output libout/libpikabinder.h
-cp target/release/libpikabinder.a libout
+cbindgen --config cbindgen.toml --crate rust-msc --output libpikabinder/libpikabinder.h
+cp target/release/libpikabinder.a libpikabinder
 cp target/release/rust-msc rust-msc-latest-linux
 chmod +x rust-msc-latest-linux
+
+cd ../pikaByteCodeGen
+cp ../pikaCompiler/libpikabinder . -r
+cp ../../src pikascript/pikascript-core -r
+rm build -rf
+mkdir build
+cd build
+cmake ..
+make -j
