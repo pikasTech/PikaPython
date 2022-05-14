dfe4d9b513093688637ea68ec5b0b8d78828e481
lyon <liang6516@outlook.com>
2022-05-14 23:44:44 +0800

    run compiling befor bind

--- tools/pikaByteCodeGen/main.c
+++ tools/pikaByteCodeGen/main.c
@@ -11,11 +11,12 @@
 PikaObj* __pikaMain;
 
 void main() {
-    /* run pika_binder to bind C modules */
-    pika_binder();
     PikaMaker* maker = New_PikaMaker();
     pikaMaker_compileModuleWithDepends(maker, "main");
     pikaMaker_linkCompiledModules(maker, "pikaModules.py.a");
     obj_deinit(maker);
+
+    /* run pika_binder to bind C modules */
+    pika_binder();
     return;
 }
