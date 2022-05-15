4a8d13124166c3cb31f358602403283452734bfb
lyon <liang6516@outlook.com>
2022-05-15 13:41:51 +0800

    restore machine.py

---
+++ bsp/stm32g070cb/pikascript/machine.py
@@ -0,0 +1,44 @@
+import STM32G0
+import PikaPiZero
+
+
+class GPIO(STM32G0.GPIO):
+    pass
+
+
+class Time(STM32G0.Time):
+    pass
+
+
+class ADC(STM32G0.ADC):
+    pass
+
+
+class UART(STM32G0.UART):
+    pass
+
+
+class PWM(STM32G0.PWM):
+    pass
+
+
+class Task(STM32G0.Task):
+    pass
+
+class IIC(STM32G0.IIC):
+    pass
+
+class RGB(PikaPiZero.RGB):
+    pass
+
+
+class KEY(PikaPiZero.KEY):
+    pass
+
+
+class LCD(PikaPiZero.LCD):
+    pass
+
+
+class Point(PikaPiZero.Point):
+    pass
