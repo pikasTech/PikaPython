bbc09813e61499a9c958ded0e4c2d70311403448
lyon <liang6516@outlook.com>
2022-05-23 22:08:55 +0800

    add comment at auto_gen lib_array.c

--- src/PikaCompiler.c
+++ src/PikaCompiler.c
@@ -345,6 +345,8 @@ int Lib_loadLibraryFileToArray(char* origin_file_name, char* out_folder) {
     char* array_name = strsGetLastToken(&buffs, origin_file_name, '/');
     array_name = strsReplace(&buffs, array_name, ".", "_");
     __platform_printf("    loading %s[]...\n", array_name);
+    pika_fputs("/* warning: auto generated file, please do not modify */\n",
+               fp);
     pika_fputs("__attribute__((aligned(4))) const unsigned char ", fp);
     pika_fputs(array_name, fp);
     pika_fputs("[] = {", fp);
