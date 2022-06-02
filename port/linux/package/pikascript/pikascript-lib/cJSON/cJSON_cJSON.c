b016924634a103e45f17826dde6b0e0dfc34735c
pikastech <liang6516@outlook.com>
2022-06-02 16:34:30 +0800

    add cJSON-test.cpp

--- port/linux/package/pikascript/pikascript-lib/cJSON/cJSON_cJSON.c
+++ port/linux/package/pikascript/pikascript-lib/cJSON/cJSON_cJSON.c
@@ -1,8 +1,9 @@
 #include "cJSON_cJSON.h"
+#include "cJSON.h"
 
 void cJSON_cJSON___init__(PikaObj* self) {}
-Arg* cJSON_cJSON_parse(PikaObj* self, char* val) {
-    return NULL;
+Arg* cJSON_cJSON_parse(PikaObj* self, char* value) {
+    cJSON* cJSON_struct = cJSON_Parse(value);
 }
 char* cJSON_cJSON_print(PikaObj* self) {
     return NULL;
---
+++ port/linux/test/cJSON-test.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+#include "test_common.h"
+extern "C" {
+#include "PikaMain.h"
+#include "PikaParser.h"
+#include "PikaStdLib_MemChecker.h"
+#include "PikaVM.h"
+#include "dataArgs.h"
+#include "dataMemory.h"
+#include "dataStrs.h"
+#include "pikaScript.h"
+#include "pika_config_gtest.h"
+}
+
+extern PikaMemInfo pikaMemInfo;
