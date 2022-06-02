5120da9ce0b8cfe41b2c7f1e0b4f3f305148491d
lyon <liang6516@outlook.com>
2022-06-02 14:26:57 +0800

    update cJSON package

---
+++ package/cJSON/cJSON_Utils.c
@@ -0,0 +1,4 @@
+#include "cJSON_Utils.h"
+
+PikaObj* cJSON_Utils_parse(PikaObj* self) {}
+PikaObj* cJSON_Utils_createObject(PikaObj *self){}
--- package/cJSON/cJSON_cJSON.c
+++ package/cJSON/cJSON_cJSON.c
@@ -1,3 +1,9 @@
 #include "cJSON_cJSON.h"
 
-void cJSON_cJSON___init__(PikaObj* self) {}
+char* cJSON_cJSON_print(PikaObj *self){
+
+}
+
+void cJSON_cJSON___init__(PikaObj *self){
+
+}
