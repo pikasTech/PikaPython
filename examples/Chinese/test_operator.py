cc62d1fefc1cf66d61c6142e5d95b70f8c4abef1
lyon <liang6516@outlook.com>
2022-06-07 15:40:27 +0800

    add chinese test

---
+++ examples/Chinese/test_operator.py
@@ -0,0 +1,50 @@
+def test():
+
+    #算术运算符
+    a = 21
+    b = 10
+    c = 0
+    
+    c = a + b
+    print("1 - c 的值为：")
+    print(c)
+    
+    c = a - b
+    print("2 - c 的值为：") 
+    print(c) 
+    
+    c = a * b
+    print("3 - c 的值为：")
+    print(c) 
+    
+    c = a / b
+    print("4 - c 的值为：")
+    print(c) 
+    
+    c = a % b
+    print("5 - c 的值为：")
+    print(c)
+    
+    # 修改变量 a 、b 、c
+    a = 2
+    b = 3
+    c = a**b 
+    print("6 - c 的值为：")
+    print(c)
+    
+    a = 10
+    b = 5
+    c = a//b 
+    print ("7 - c 的值为：")
+    print(c)
+
+
+
+    #比较运算符
+    a = 21
+    b = 10
+    c = 0
+    if  a == b :
+        print ("1 - a 等于 b")
+    else:
+        print("1 - a 不等于 b")
