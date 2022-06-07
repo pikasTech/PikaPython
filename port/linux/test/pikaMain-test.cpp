bd3a4d1067e1ec2ab2924b72c855782b2a926f0f
pikastech <liang6516@outlook.com>
2022-06-07 14:22:02 +0800

    run example/UnitTest/main.py in gtest

--- port/linux/test/pikaMain-test.cpp
+++ port/linux/test/pikaMain-test.cpp
@@ -34,6 +34,21 @@ TEST(pikaMain, new_mem) {
     EXPECT_EQ(pikaMemNow(), 0);
 }
 
+TEST(pikaMain, unitTest) {
+    /* init */
+    pikaMemInfo.heapUsedMax = 0;
+    PikaObj* pikaMain = newRootObj("pikaMain", New_PikaMain);
+    /* run */
+    __platform_printf("BEGIN\r\n");
+    pikaVM_runFile(pikaMain, "../../examples/UnitTest/main.py");
+    /* collect */
+    /* assert */
+    /* deinit */
+    obj_deinit(pikaMain);
+
+    EXPECT_EQ(pikaMemNow(), 0);
+}
+
 TEST(pikaMain, list_new) {
     /* init */
     pikaMemInfo.heapUsedMax = 0;
