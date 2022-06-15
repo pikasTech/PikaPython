002d3f754207e65505045272858f0da86bf008b5
lyon <liang6516@outlook.com>
2022-06-15 20:38:14 +0800

    add lvgl-simulation

---
+++ bsp/lvgl-vs-simu/LVGL.Simulator/pikascript/main.py
@@ -0,0 +1,12 @@
+import pika_lvgl as lv
+import PikaStdLib
+mem = PikaStdLib.MemChecker()
+
+# Create an Arc
+arc = lv.arc(lv.scr_act())
+arc.set_end_angle(200)
+arc.set_size(150, 150)
+arc.center()
+
+print('mem used max: %0.2f kB' % (mem.getMax()))
+print('mem used now: %0.2f kB' % (mem.getNow()))
