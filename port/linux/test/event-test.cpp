dbeb9ab239dd41943dba3969ee77208da36b6037
pikastech <liang6516@outlook.com>
2022-06-17 14:45:52 +0800

    add test and example for event

---
+++ port/linux/test/event-test.cpp
@@ -0,0 +1,31 @@
+#include "test_common.h"
+
+
+extern PikaEventListener* g_pika_device_event_listener;
+TEST(event, gpio) {
+    /* init */
+    PikaObj* pikaMain = newRootObj("pikaMain", New_PikaMain);
+    pks_eventLisener_init(&g_pika_device_event_listener);
+
+    /* run */
+    pikaVM_runFile(pikaMain, "../../examples/TemplateDevice/gpio_cb.py");
+
+    #define EVENT_SIGAL_IO_RISING_EDGE 0x01
+    #define EVENT_SIGAL_IO_FALLING_EDGE 0x02
+    #define GPIO_PA8_EVENT_ID 0x08
+
+    /* simulate run in the call back */
+    pks_eventLisener_sendSignal(g_pika_device_event_listener, GPIO_PA8_EVENT_ID,
+                            EVENT_SIGAL_IO_RISING_EDGE);
+    pks_eventLisener_sendSignal(g_pika_device_event_listener, GPIO_PA8_EVENT_ID,
+                            EVENT_SIGAL_IO_FALLING_EDGE);
+    /* collect */
+    /* assert */
+    EXPECT_STREQ(log_buff[1], "get rising edge!\r\n");
+    EXPECT_STREQ(log_buff[0], "get falling edge!\r\n");
+    /* deinit */
+    obj_deinit(pikaMain);
+    pks_eventLisener_deinit(&g_pika_device_event_listener);
+
+    EXPECT_EQ(pikaMemNow(), 0);
+}
