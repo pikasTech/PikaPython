8b71911be4e7a5d3d2a84bf8ae41c1f8b6675959
lyon <liang6516@outlook.com>
2022-06-21 14:58:49 +0800

    improve build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 DOCKER_NAME=pikadev
 
 docker rmi $DOCKER_NAME
 
-docker build -t $DOCKER_NAME .
+if [ $# == 0 ]
+then
+    docker build -t $DOCKER_NAME . 
+fi
+
+if [ $# == 1 ]
+then
+    docker build $1 -t $DOCKER_NAME . 
+fi
