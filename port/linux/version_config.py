1578eacdea69766dcebc2017a99c4fade19ef53f
pikastech <liang6516@outlook.com>
2022-08-13 10:41:49 +0800

    release v1.10.1

--- port/linux/version_config.py
+++ port/linux/version_config.py
@@ -1,3 +1,3 @@
 MajorVersion  =   "1"
 MinorVersion  =   "10"
-MicroVersion  =   "0"
+MicroVersion  =   "1"
--- src/PikaVersion.h
+++ src/PikaVersion.h
@@ -1,5 +1,5 @@
 #define PIKA_VERSION_MAJOR       1
 #define PIKA_VERSION_MINOR       10
-#define PIKA_VERSION_MICRO       0
+#define PIKA_VERSION_MICRO       1
 
-#define PIKA_EDIT_TIME      "2022/08/12 23:51:12"
+#define PIKA_EDIT_TIME      "2022/08/13 10:41:48"
