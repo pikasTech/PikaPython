34ad50ffe846154ec9b7bd2f8331f745d8b489df
pikastech <liang6516@outlook.com>
2022-08-24 13:18:38 +0800

    fix macro in stddevice

--- port/linux/package/pikascript/PikaStdDevice.pyi
+++ port/linux/package/pikascript/PikaStdDevice.pyi
@@ -102,31 +102,31 @@ class Time(BaseDev):
     def sleep(self, s: float):
         """Sleep for s seconds."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def time(self) -> float:
         """Get the current time."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def time_ns(self) -> int:
         """Get the current time in nanoseconds."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def gmtime(self, unix_time: float):
         """Convert unix time to struct_time."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def localtime(self, unix_time: float):
         """Convert unix time to struct_time."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def mktime(self) -> int:
         """Convert struct_time to unix time."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def asctime(self):
         """Convert struct_time to string."""
 
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def ctime(self, unix_time: float):
         """Convert unix time to string."""
 
@@ -137,7 +137,7 @@ class Time(BaseDev):
     def sleep_ms(self, ms: int): ...
 
     @abstractmethod
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_STD_DEVICE_UNIX_TIME_ENABLE")
     def platformGetTick(): ...
 
 
@@ -450,10 +450,10 @@ class CAN(BaseDev):
 
 
 class BaseDev:
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_EVENT_ENABLE")
     def addEventCallBack(self, eventCallback: any): 
         """ Add an event callback. """
 
     @abstractmethod
-    @PIKA_C_MACRO_IF("!PIKA_NANO_ENABLE")
+    @PIKA_C_MACRO_IF("PIKA_EVENT_ENABLE")
     def platformGetEventId(self): ...
