1f122e3a2a7cc73e9cdd7da1f08ae92009e34983
eglwang <eglwang@163.com>
2022-08-29 12:47:05 +0800

    append re.pyi

---
+++ package/re/re.pyi
@@ -0,0 +1,54 @@
+from PikaObj import *
+
+I: int
+IGNORECASE:int
+M: int
+MULTILINE:int
+DOTALL: int
+
+
+class Pattern():
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def findall(self, subject: str, *flags) -> list:
+        pass
+
+    def sub(self, repl: str, subjet: str, *flags) -> str:
+        pass
+
+    def match(self, subject: str, *flags) -> Match:
+        pass
+    def fullmatch(self, subject: str, *flags) -> Match:
+        pass
+
+    def search(self, subject: str, *flags) -> Match:
+        pass
+
+
+class Match():
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    def group(self, n: int) -> str:
+        pass
+
+    def groups(self) -> list:
+        pass
+
+    def span(self, group_n: int) -> list:
+        pass
+
+
+def findall(pattern: str, subject: str, *flags) -> list:...
+def sub(pattern: str, repl: str, subjet: str, *flags) -> str: ...
+def match(pattern: str, subject: str, *flags) -> Match: ...
+def fullmatch(pattern: str, subject: str, *flags) -> Match: ...
+def search(pattern: str, subject: str, *flags) -> Match: ...
+def compile(pattern: str) -> Pattern: ...
