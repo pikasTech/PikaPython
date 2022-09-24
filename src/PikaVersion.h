dd73fa7b5a5f2feccce180182ffdb027a1a754b6
pikastech <liang6516@outlook.com>
2022-09-24 20:58:24 +0800

    ready to release v1.11.2

--- src/PikaVersion.h
+++ src/PikaVersion.h
@@ -1,5 +1,5 @@
 #define PIKA_VERSION_MAJOR       1
 #define PIKA_VERSION_MINOR       11
-#define PIKA_VERSION_MICRO       1
+#define PIKA_VERSION_MICRO       2
 
-#define PIKA_EDIT_TIME      "2022/09/24 13:54:51"
+#define PIKA_EDIT_TIME      "2022/09/24 20:58:10"
