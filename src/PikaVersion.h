b5200942f8af9ffcbf2168232eb56871015d2279
pikastech <liang6516@outlook.com>
2022-10-15 23:34:08 +0800

    update version

--- src/PikaVersion.h
+++ src/PikaVersion.h
@@ -1,5 +1,5 @@
 #define PIKA_VERSION_MAJOR       1
 #define PIKA_VERSION_MINOR       11
-#define PIKA_VERSION_MICRO       4
+#define PIKA_VERSION_MICRO       5
 
-#define PIKA_EDIT_TIME      "2022/10/15 23:32:32"
+#define PIKA_EDIT_TIME      "2022/10/15 23:34:06"
