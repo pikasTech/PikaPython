4d51e26c27b12645116416d34dfae2e5ed70d633
李昂 <liang6516@outlook.com>
2022-11-02 05:08:44 +0000

    update  bsp/lvgl-vs-simu/LVGL.Simulator/lv_conf.h.
    Signed-off-by: 李昂 <liang6516@outlook.com>

--- bsp/lvgl-vs-simu/LVGL.Simulator/lv_conf.h
+++ bsp/lvgl-vs-simu/LVGL.Simulator/lv_conf.h
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * @file lv_conf.h
  * Configuration file for v8.1.1-dev
  */
@@ -558,7 +558,7 @@
 #define LV_USE_FS_FATFS '\0'        /*Uses f_open, f_read, etc*/
 
 /*PNG decoder library*/
-#define LV_USE_PNG 0
+#define LV_USE_PNG 1
 
 /*BMP decoder library*/
 #define LV_USE_BMP 0
