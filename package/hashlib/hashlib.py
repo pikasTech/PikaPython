a91c3698abce8a6624929243cfb7cc79badaa156
dreamcmi <1912544842@qq.com>
2022-11-19 18:37:01 +0800

    optimize hashlib package

--- package/hashlib/hashlib.py
+++ package/hashlib/hashlib.py
@@ -3,41 +3,41 @@ import _hashlib
 
 def new(mode: str) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.new(mode)
+    hash.new(mode, None)
     return hash
 
 
 def md5(data = None) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.md5(data)
+    hash.new("md5", data)
     return hash
 
 
 def sha1(data = None) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.sha1(data)
+    hash.new("sha1", data)
     return hash
 
 
 def sha224(data = None) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.sha224(data)
+    hash.new("sha224", data)
     return hash
 
 
 def sha256(data = None) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.sha256(data)
+    hash.new("sha256", data)
     return hash
 
 
 def sha384(data = None) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.sha384(data)
+    hash.new("sha384", data)
     return hash
 
 
 def sha512(data = None) -> _hashlib.Hash:
     hash = _hashlib.Hash()
-    hash.sha512(data)
+    hash.new("sha512", data)
     return hash
