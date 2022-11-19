50b995c5e4a5beb02220c989f592affba8a9cf89
lyon <liang6516@outlook.com>
2022-11-19 19:43:29 +0800

    fix version

--- port/linux/version_config.py
+++ port/linux/version_config.py
@@ -1,3 +1,3 @@
 MajorVersion  =   "1"
 MinorVersion  =   "11"
-MicroVersion  =   "6"
+MicroVersion  =   "7"
--- src/PikaVersion.h
+++ src/PikaVersion.h
@@ -1,5 +1,5 @@
 #define PIKA_VERSION_MAJOR       1
 #define PIKA_VERSION_MINOR       11
-#define PIKA_VERSION_MICRO       6
+#define PIKA_VERSION_MICRO       7
 
-#define PIKA_EDIT_TIME      "2022/11/16 21:22:16"
+#define PIKA_EDIT_TIME      "2022/11/19 19:43:26"
