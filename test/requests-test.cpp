75bdfeccf6a1a24a83b0a07455a4a066d49293ff
lyon <liang6516@outlook.com>
2022-11-24 01:07:39 +0800

    merge master

--- test/requests-test.cpp
+++ test/requests-test.cpp
@@ -243,4 +243,19 @@ TEST(requests, std_request) {
     EXPECT_EQ(pikaMemNow(), 0);
 }
 
+TEST(requests, rest_init) {
+    PikaObj* pikaMain = newRootObj("pikaMain", New_PikaMain);
+    extern unsigned char pikaModules_py_a[];
+    obj_linkLibrary(pikaMain, pikaModules_py_a);
+    obj_run(pikaMain, 
+    "import requests\n"
+    "r = requests.Response()\n"
+    );
+    /* assert */
+    /* deinit */
+    obj_deinit(pikaMain);
+    EXPECT_EQ(pikaMemNow(), 0);
+}
+
+
 #endif
