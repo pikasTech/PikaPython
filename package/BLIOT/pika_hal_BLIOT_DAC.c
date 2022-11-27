4599ce8681d293a5cc7682115205ab5e85e9b5e8
lyon <liang6516@outlook.com>
2022-11-27 19:33:48 +0800

    add_files

---
+++ package/BLIOT/pika_hal_BLIOT_DAC.c
@@ -0,0 +1,29 @@
+#include "../PikaStdDevice/pika_hal.h"
+int pika_hal_platform_DAC_open(pika_dev* dev, char* name) {
+    return -1;
+}
+
+int pika_hal_platform_DAC_close(pika_dev* dev) {
+    return -1;
+}
+
+int pika_hal_platform_DAC_read(pika_dev* dev, void* buf, size_t count) {
+    return -1;
+}
+
+int pika_hal_platform_DAC_write(pika_dev* dev, void* buf, size_t count) {
+    return -1;
+}
+
+int pika_hal_platform_DAC_ioctl_enable(pika_dev* dev) {
+    return -1;
+}
+
+int pika_hal_platform_DAC_ioctl_disable(pika_dev* dev) {
+    return -1;
+}
+
+int pika_hal_platform_DAC_ioctl_config(pika_dev* dev,
+                                       pika_hal_DAC_config* cfg) {
+    return -1;
+}
