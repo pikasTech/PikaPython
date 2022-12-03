1a66a70893702174294214d227aee3ec8e4498af
lyon <liang6516@outlook.com>
2022-12-03 19:04:28 +0800

    update pre-compiler
    update version

--- src/PikaVersion.h
+++ src/PikaVersion.h
@@ -1,5 +1,5 @@
 #define PIKA_VERSION_MAJOR       1
 #define PIKA_VERSION_MINOR       11
-#define PIKA_VERSION_MICRO       7
+#define PIKA_VERSION_MICRO       8
 
-#define PIKA_EDIT_TIME      "2022/11/19 19:43:26"
+#define PIKA_EDIT_TIME      "2022/12/03 19:05:57"
