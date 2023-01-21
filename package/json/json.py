02d6151cac594f2278c04e616f03cf37bae3069b
lyon <liang6516@outlook.com>
2023-01-21 17:17:28 +0800

    support json.dumps()

--- package/json/json.py
+++ package/json/json.py
@@ -1,7 +1,7 @@
 import pika_cjson as cjson
 
 
-def _cjson_encode(cjson):
+def _cjson_encode(cjson: cjson.cJSON):
     if cjson == None:
         return None
     elif cjson.isInvalid():
@@ -38,3 +38,35 @@ def _cjson_encode(cjson):
 def loads(json: str) -> dict:
     cj = cjson.Parse(json)
     return _cjson_encode(cj)
+
+
+def _cjson_decode(d: dict):
+    if d == None:
+        return cjson.Null()
+    elif str(type(d)) == "<class 'int'>":
+        return cjson.Number(d)
+    elif str(type(d)) == "<class 'float'>":
+        return cjson.Number(d)
+    elif str(type(d)) == "<class 'bool'>":
+        if d:
+            return cjson.True_()
+        else:
+            return cjson.False_()
+    elif str(type(d)) == "<class 'str'>":
+        return cjson.String(d)
+    elif str(type(d)) == "<class 'list'>":
+        res = cjson.Array()
+        for i in d:
+            res.addItemToArray(_cjson_decode(i))
+        return res
+    elif str(type(d)) == "<class 'dict'>":
+        res = cjson.Object()
+        for k, v in d.items():
+            res.addItemToObject(k, _cjson_decode(v))
+        return res
+    else:
+        return cjson.Null()
+
+
+def dumps(d: dict) -> str:
+    return _cjson_decode(d).print()
---
+++ test/python/json/json_dumps.py
@@ -0,0 +1,58 @@
+import json
+
+s1 = json.dumps({"a": 1, "b": 2, "c": 3})
+print(s1)
+
+s2 = json.dumps(
+    {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": {
+            "e": 4,
+            "f": 5
+        }
+    }
+)
+print(s2)
+
+s3 = json.dumps(
+    {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": {
+            "e": 4,
+            "f": 5
+        },
+        "g": [
+            6,
+            7,
+            8
+        ]
+    }
+)
+print(s3)
+
+s4 = json.dumps(
+    {
+        "a": 1,
+        "b": 2,
+        "c": 3,
+        "d": {
+            "e": 4,
+            "f": 5
+        },
+        "g": [
+            6,
+            7,
+            8
+        ],
+        "h": None,
+        "i": False,
+        "j": True,
+        "k": "string",
+        "l": 1.234
+    }
+)
+print(s4)
