3df11749dd53216f1c65c34ccc5bee8bb7225e82
lyon <liang6516@outlook.com>
2023-02-28 10:59:53 +0800

    add thread.self test

--- test/thread-test.cpp
+++ test/thread-test.cpp
@@ -17,5 +17,21 @@ TEST(thread, test1) {
     EXPECT_EQ(pikaMemNow(), 0);
 }
 
+TEST(thread, self) {
+    pikaMemInfo.heapUsedMax = 0;
+    PikaObj* pikaMain = newRootObj("pikaMain", New_PikaMain);
+    extern unsigned char pikaModules_py_a[];
+    obj_linkLibrary(pikaMain, pikaModules_py_a);
+    /* run */
+    __platform_printf("BEGIN\r\n");
+    pikaVM_runSingleFile(pikaMain, "test/python/_thread/thread_self.py");
+    /* clang-format on */
+    /* deinit */
+    obj_deinit(pikaMain);
+
+    EXPECT_STREQ(log_buff[0], "self._val: 2\r\n");
+    EXPECT_EQ(pikaMemNow(), 0);
+}
+
 #endif
 TEST_END
