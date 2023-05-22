974a3619f7478f575fcce4fcc9f056f090f2107f
pikastech <liang6516@outlook.com>
2023-05-22 17:58:06 +0800

    _

--- test/lua-test.cpp
+++
@@ -1,9 +0,0 @@
-#include "test_common.h"
-TEST_START
-#if !PIKA_NANO_ENABLE
-
-TEST_RUN_SINGLE_FILE_PASS(lua, eval, "test/python/pika_lua/eval.py")
-TEST_RUN_SINGLE_FILE_PASS(lua, require, "test/python/pika_lua/require.py")
-
-#endif
-TEST_END
