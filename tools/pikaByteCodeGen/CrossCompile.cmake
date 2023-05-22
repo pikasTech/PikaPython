6d8a7b9e7e0220b8cec3afe8dd802ab9757e820b
pikastech <liang6516@outlook.com>
2023-05-22 19:54:26 +0800

    _

--- tools/pikaByteCodeGen/CrossCompile.cmake
+++
@@ -1,5 +0,0 @@
-set(CMAKE_SYSTEM_NAME WindowsStore)
-
-set(tools /usr/bin)
-set(CMAKE_C_COMPILER /usr/bin/i686-w64-mingw32-gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/i686-w64-mingw32-g++)
--- tools/pikaByteCodeGen/cross-win.sh
+++
@@ -1,3 +0,0 @@
-rm build-win -rf && \
-mkdir build-win && \
-cd build-win && cmake -DCMAKE_TOOLCHAIN_FILE=../CrossCompile.cmake -DCROSS_COMPILE=ON .. && make -j
