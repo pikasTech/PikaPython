eb9a4c74941c97df7a12e9b49b66e812a8ff332a
Lyon <liang6516@outlook.com>
2023-07-21 20:03:30 +0800

    fix socket_GET example

--- examples/socket/socket_GET.py
+++ examples/socket/socket_GET.py
@@ -4,20 +4,23 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # 获取服务器的IP地址
-server_ip = socket.gethostbyname('pikapython.com')
+server_ip = socket.gethostbyname('baidu.com')
 server_port = 80
 
 # 连接到服务器
 s.connect((server_ip, server_port))
-
 # 创建HTTP GET请求
-request = 'GET / HTTP/1.1\r\nHost: pikapython.com\r\n\r\n'
+request = 'GET / HTTP/1.1\r\nHost: baidu.com\r\n\r\n'
+# print('request:', request)
 s.send(request.encode())
 
 # 接收服务器的响应
 response = ''
 while True:
-    recv = s.recv(1024)
+    try:
+        recv = s.recv(1024)
+    except:
+        break
     if not recv:
         break
     response += recv.decode()
