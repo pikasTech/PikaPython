9fdf0976c87c99447f9e16a9e48cb0df16937151
lyon1998 <liang6516@outlook.com>
2024-03-11 09:01:21 +0800

    fix rtt plat

--- src/PikaPlatform.c
+++ src/PikaPlatform.c
@@ -83,7 +83,9 @@ static int gettimeofday(struct timeval* tp, void* tzp) {
     return (0);
 }
 
-static void timeradd(struct timeval* a, struct timeval* b, struct timeval* res) {
+static void timeradd(struct timeval* a,
+                     struct timeval* b,
+                     struct timeval* res) {
     res->tv_sec = a->tv_sec + b->tv_sec;
     res->tv_usec = a->tv_usec + b->tv_usec;
     if (res->tv_usec >= 1000000) {
@@ -92,7 +94,9 @@ static void timeradd(struct timeval* a, struct timeval* b, struct timeval* res) 
     }
 }
 
-static void timersub(struct timeval* a, struct timeval* b, struct timeval* res) {
+static void timersub(struct timeval* a,
+                     struct timeval* b,
+                     struct timeval* res) {
     res->tv_sec = a->tv_sec - b->tv_sec;
     res->tv_usec = a->tv_usec - b->tv_usec;
     if (res->tv_usec < 0) {
