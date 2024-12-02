b4c2d2da035f38137d6dd41e2509454c42880a43
lyon <liang6516@outlook.com>
2024-12-02 20:55:15 +0800

    add timer.py based on _thread and add example

---
+++ examples/_thread/timer1.py
@@ -0,0 +1,22 @@
+import time
+from timer import Timer
+
+
+tim = Timer()
+
+
+def task1(a, b):
+    print('task1:', a, b)
+
+
+def task2(a, b):
+    print('task2:', a, b)
+
+
+tim.call_after_ms(1000, task1, (1, 2))
+tim.call_after_ms(2000, task2, (3, 4))
+
+while len(tim.tasks) > 0:
+    time.sleep(1)
+
+tim.stop()
---
+++ port/linux/package/pikascript/timer.py
@@ -0,0 +1,36 @@
+import _thread
+import time
+
+class Timer:
+    def __init__(self, tick_ms=10):
+        self.tick_ms = tick_ms
+        self.is_running = False
+        self.stoped = False
+        self.tasks = []
+        self.debug = False
+        self.start()
+
+    def start(self):
+        self.is_running = True
+        _thread.start_new_thread(self.run, ())
+
+    def run(self):
+        while self.is_running:
+            time.sleep_ms(self.tick_ms)
+            for task in self.tasks:
+                time_end, func, args = task
+                if time.tick_ms() >= time_end:
+                    func(*args)
+                    self.tasks.remove(task)
+                    if self.debug:
+                        print('tasks after run:', len(self.tasks))
+        self.stoped = True
+
+    def stop(self):
+        self.is_running = False
+        while not self.stoped:
+            time.sleep_ms(self.tick_ms)
+
+    def call_after_ms(self, ms, func, args=()):
+        time_end = time.tick_ms() + ms
+        self.tasks.append((time_end, func, args))
