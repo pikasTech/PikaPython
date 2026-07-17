b8e03f595eb90ada7317180da2bbec51b296f498
pikastech <liang6516@outlook.com>
2026-07-17 23:40:28 +0200

    ci: bound and isolate Linux core tests

--- port/linux/_gtest_once.sh
+++ port/linux/_gtest_once.sh
@@ -1,4 +1,6 @@
-# sh update-compiler.sh
-rm build/test/pikascript_test -f
+set -e
+
+rm -f build/test/pikascript_test
 sh make.sh
-build/test/pikascript_test
+PIKA_GTEST_FILTER=${PIKA_GTEST_FILTER:--mqtt.*:network.*:socket.*:requests.*:jrpc.BlockingRequestBetweenTwoJRPC}
+timeout 10m build/test/pikascript_test --gtest_filter="$PIKA_GTEST_FILTER"
